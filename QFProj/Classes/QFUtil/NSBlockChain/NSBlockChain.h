c2b4b13792b820a4264f3fa9c16e0ed78a7229ee
dqf <897432098@qq.com>
2018-06-26 14:33:07 +0800

    增加新的功能

---
+++ QFProj/Classes/QFUtil/NSBlockChain/NSBlockChain.h
@@ -0,0 +1,23 @@
+//
+//  NSBlockChain.h
+//  TestProject
+//
+//  Created by dqf on 2018/6/26.
+//  Copyright © 2018年 dqf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSBlock : NSObject
+@property (nonatomic) NSInteger index;
+@property (nonatomic) NSString  *dateCreated;
+@property (nonatomic) NSString  *previousHash;
+@property (nonatomic) NSString  *hashString;
+@property (nonatomic) NSInteger nonce;
+@end
+
+@interface NSBlockChain : NSObject
+- (id)initWithBlock:(NSBlock *)genesisBlock;
+- (void)addBlock:(NSBlock *)block;
+- (NSArray *)allBlocks;
+@end
