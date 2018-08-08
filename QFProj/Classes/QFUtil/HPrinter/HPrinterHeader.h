e71bc9c80ce1d2247e0fb1b3cf8601c9b89f2519
dqf <897432098@qq.com>
2018-08-08 09:16:38 +0800

    增加新的功能

---
+++ QFProj/Classes/QFUtil/HPrinter/HPrinterHeader.h
@@ -0,0 +1,15 @@
+//
+//  HPrinterHeader.h
+//  QFProj
+//
+//  Created by dqf on 2018/8/8.
+//  Copyright © 2018年 dqfStudio. All rights reserved.
+//
+
+#ifndef HPrinterHeader_h
+#define HPrinterHeader_h
+
+#import "UIView+HPrinter.h"
+#import "UIViewController+HPrinter.h"
+
+#endif /* HPrinterHeader_h */
