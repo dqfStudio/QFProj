0933d0ece70292279af56b530ba84656d75fda22
dqf <897432098@qq.com>
2018-08-16 17:20:28 +0800

    增加获取权限的功能

---
+++ QFProj/Classes/QFUtil/HAuthorizeManager/HAuthorizeManager.h
@@ -0,0 +1,42 @@
+//
+//  HAuthorizeManager.h
+//  QFProj
+//
+//  Created by dqf on 2018/8/16.
+//  Copyright © 2018年 dqfStudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger){
+    AuthorizationTypeCamera,   //相机
+    AuthorizationTypeAudio,    //麦克风
+    AuthorizationTypeLocation,  //位置
+    AuthorizationTypePhotoLibrary,  //相册
+    AuthorizationTypeContacts,     //通讯录
+}AuthorizationType;
+
+typedef NS_ENUM(NSInteger){
+    AuthorizationStatusAuthorized,
+    AuthorizationStatusDenied,
+}AuthorizationStatus;
+
+typedef void(^AuthorizationCompletionBlock)(AuthorizationStatus status);
+
+#define KCHECK_AUTH_CAMERA        @"请在iPhone的“设置”-“隐私”-“相机”功能中，找到“Enigma”打开相机访问权限"
+#define KCHECK_AUTH_PHOTOLIB      @"请在iPhone的“设置”-“隐私”-“照片”功能中，找到“Enigma”打开相册访问权限"
+#define KCHECK_AUTH_MICROPHONE    @"请在iPhone的“设置”-“隐私”-“麦克风”功能中，找到“Enigma”打开麦克风访问权限"
+#define KCHECK_AUTH_CONTACT       @"请在iPhone的“设置”-“隐私”-“通讯录”功能中，找到“Enigma”打开通讯录访问权限"
+#define KCHECK_AUTH_LOCATION      @"请在iPhone的“设置”-“隐私”-“位置”功能中，找到“Enigma”打开位置访问权限"
+
+@interface HAuthorizeManager : NSObject
+
+@property (nonatomic, copy) AuthorizationCompletionBlock authorizationCompletionBlock;
+
+//获取权限的状态
++ (void)getAutorizationStatusWithType:(AuthorizationType)authorizationType completion:(AuthorizationCompletionBlock)completion;
+
+//没有权限的提示
++ (void)showAlertWithAuthorizationType:(AuthorizationType)authorizationType;
+
+@end
---
+++ QFProj/Classes/QFUtil/HAuthorizeManager/HAuthorizeManager.m
@@ -0,0 +1,201 @@
+//
+//  HAuthorizeManager.m
+//  QFProj
+//
+//  Created by dqf on 2018/8/16.
+//  Copyright © 2018年 dqfStudio. All rights reserved.
+//
+
+#import "HAuthorizeManager.h"
+#import <AVFoundation/AVFoundation.h>
+#import <Photos/Photos.h>
+#import <CoreLocation/CoreLocation.h>
+#import <Contacts/Contacts.h>
+
+#define dispatch_main_async_safe(block)\
+if ([NSThread isMainThread]) {\
+block();\
+} else {\
+dispatch_async(dispatch_get_main_queue(), block);\
+}
+
+@interface HAuthorizeManager()<CLLocationManagerDelegate>
+@property (nonatomic)CLLocationManager *locationManager;
+@end
+
+@implementation HAuthorizeManager
+
++ (instancetype)sharemanager {
+    static HAuthorizeManager *manager;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        manager = [[self alloc] init];
+        manager.locationManager = [[CLLocationManager alloc] init];
+        manager.locationManager.delegate = manager;
+        [manager.locationManager requestWhenInUseAuthorization];
+    });
+    return manager;
+}
+
++ (void)authorizeAll
+{
+    //定位权限
+    [[self class] sharemanager];
+    //相机权限
+    [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+    }];
+    //麦克风权限
+    [AVCaptureDevice requestAccessForMediaType:AVMediaTypeAudio completionHandler:^(BOOL granted) {
+    }];
+    //相册权限
+    [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+    }];
+    //通讯录
+    CNContactStore *contactStore = [[CNContactStore alloc] init];
+    [contactStore requestAccessForEntityType:CNEntityTypeContacts completionHandler:^(BOOL granted, NSError * _Nullable error) {
+    }];
+}
+
++ (void)getAutorizationStatusWithType:(AuthorizationType)authorizationType completion:(AuthorizationCompletionBlock)completion
+{
+    switch (authorizationType) {
+        case AuthorizationTypeCamera:
+        {
+            [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+                if (granted) {
+                    completion(AuthorizationStatusAuthorized);
+                }else{
+                    completion(AuthorizationStatusDenied);
+                }
+            }];
+        }
+            break;
+        case AuthorizationTypeAudio:
+        {
+            [AVCaptureDevice requestAccessForMediaType:AVMediaTypeAudio completionHandler:^(BOOL granted) {
+                if (granted) {
+                    completion(AuthorizationStatusAuthorized);
+                }else{
+                    completion(AuthorizationStatusDenied);
+                }
+            }];
+        }
+            break;
+        case AuthorizationTypeLocation:
+        {
+            CLAuthorizationStatus status = [CLLocationManager authorizationStatus];
+            if (status == kCLAuthorizationStatusNotDetermined) {
+                [HAuthorizeManager sharemanager].authorizationCompletionBlock = completion;
+            }else if (status == kCLAuthorizationStatusAuthorizedWhenInUse || status == kCLAuthorizationStatusAuthorizedAlways){
+                completion(AuthorizationStatusAuthorized);
+            }else{
+                completion(AuthorizationStatusDenied);
+            }
+        }
+            break;
+        case AuthorizationTypePhotoLibrary:
+        {
+            [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+                if (status == PHAuthorizationStatusAuthorized) {
+                    completion(AuthorizationStatusAuthorized);
+                }else{
+                    completion(AuthorizationStatusDenied);
+                }
+            }];
+        }
+            break;
+        case AuthorizationTypeContacts:
+        {
+            //通讯录
+            CNContactStore *contactStore = [[CNContactStore alloc] init];
+            [contactStore requestAccessForEntityType:CNEntityTypeContacts completionHandler:^(BOOL granted, NSError * _Nullable error) {
+                if (granted) {
+                    completion(AuthorizationStatusAuthorized);
+                }else{
+                    completion(AuthorizationStatusDenied);
+                }
+            }];
+        }
+            break;
+            
+        default:
+            break;
+    }
+}
+
++ (void)showAlertWithAuthorizationType:(AuthorizationType)authorizationType
+{
+    NSString *title;
+    NSString *message;
+    NSString *confirmTitle = @"设置";
+    NSString *cancelTitle = @"取消";
+    dispatch_block_t gosetting = ^{
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString] options:@{} completionHandler:^(BOOL success) {
+        }];
+    };
+    switch (authorizationType) {
+        case AuthorizationTypePhotoLibrary://没有相册权限
+        {
+            title = @"没有相册权限";
+            message = KCHECK_AUTH_PHOTOLIB;
+        }
+            break;
+        case AuthorizationTypeCamera://没有相机权限
+        {
+            title = @"没有相机权限";
+            message = KCHECK_AUTH_CAMERA;
+        }
+            break;
+        case AuthorizationTypeAudio://没有麦克风权限
+        {
+            title = @"没有麦克风权限";
+            message = KCHECK_AUTH_MICROPHONE;
+        }
+            break;
+        case AuthorizationTypeContacts://没有通讯录权限
+        {
+            title = @"没有通讯录权限";
+            message = KCHECK_AUTH_CONTACT;
+        }
+            break;
+        case AuthorizationTypeLocation://没有定位权限
+        {
+            title = @"没有位置权限";
+            message = KCHECK_AUTH_LOCATION;
+        }
+            break;
+        default:
+            break;
+    }
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:cancelTitle style:UIAlertActionStyleCancel handler:nil];
+    UIAlertAction *confirm = [UIAlertAction actionWithTitle:confirmTitle style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        gosetting();
+    }];
+    [alert addAction:cancel];
+    [alert addAction:confirm];
+    
+    dispatch_main_async_safe(^{
+        [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
+    });
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status
+{
+    if (status != kCLAuthorizationStatusNotDetermined) {
+        if (status == kCLAuthorizationStatusAuthorizedWhenInUse || status == kCLAuthorizationStatusAuthorizedAlways) {
+            if (self.authorizationCompletionBlock) {
+                self.authorizationCompletionBlock(AuthorizationStatusAuthorized);
+            }
+        }else{
+            if (self.authorizationCompletionBlock) {
+                self.authorizationCompletionBlock(AuthorizationStatusDenied);
+            }
+        }
+        self.authorizationCompletionBlock = nil;
+        self.locationManager.delegate = self;
+    }
+}
+
+@end
