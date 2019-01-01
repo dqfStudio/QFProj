afb486c1b1a9e85466ae3a6b0e299c40c2f0c3ab
wind <897432098@qq.com>
2019-01-01 16:07:28 +0700

    modify some bugs

--- QFProj/Classes/QFUtil/HTupleView/Cells/HTupleViewCell/HTupleViewCell.h
+++ QFProj/Classes/QFUtil/HTupleView/Cells/HTupleViewCell/HTupleViewCell.h
@@ -10,7 +10,7 @@
 #import "HWebImageView.h"
 #import "HWebButtonView.h"
 #import "Masonry.h"
-#import "UILabel+HAttributeText.h"
+#import <YYLabel.h>
 
 @class HButtonViewCell, HImageViewCell;
 
@@ -21,24 +21,34 @@ typedef void(^HImageViewBlock)(HWebImageView *webImageView, HImageViewCell *imag
 @property (nonatomic) UIView *view;
 @end
 
-@interface HLabelViewCell : HTupleBaseCell
-@property (nonatomic) UILabel *label;
-@end
-
-@interface HTextViewCell : HTupleBaseCell
-@property (nonatomic) UITextView *textView;
-@end
-
-@interface HTextFieldCell : HTupleBaseCell
-@property (nonatomic) UITextField *textField;
-@end
-
 @interface HScrollViewCell : HTupleBaseCell
 @property (nonatomic) UIScrollView *scrollView;
 @end
 
+@interface HTextViewCell : HTupleBaseCell
+@property (nonatomic) UILabel *label;
+@end
+
+@interface HTextViewCell2 : HTupleBaseCell
+@property (nonatomic) UILabel *leftLabel;
+@property (nonatomic) UILabel *rightLabel;
+@end
+
+@interface HTextViewCell3 : HTupleBaseCell
+@property (nonatomic) UILabel *upLabel;
+@property (nonatomic) UILabel *downLabel;
+@end
+
+@interface HTextViewCell4 : HTupleBaseCell
+@property (nonatomic) UITextView *textView;
+@end
+
+@interface HTextViewCell5 : HTupleBaseCell
+@property (nonatomic) YYLabel *label;
+@end
+
 @interface HButtonViewCell : HTupleBaseCell
-@property (nonatomic) HWebButtonView *buttonView;
+@property (nonatomic) HWebButtonView *button;
 @property (nonatomic, copy) HButtonViewBlock buttonViewBlock;
 @end
 
@@ -47,12 +57,8 @@ typedef void(^HImageViewBlock)(HWebImageView *webImageView, HImageViewCell *imag
 @property (nonatomic, copy) HImageViewBlock imageViewBlock;
 @end
 
-@interface HText2ViewCell : HTupleBaseCell
-@property (nonatomic) UILabel *leftLabel;
-@property (nonatomic) UILabel *rightLabel;
+@interface HTextFieldCell : HTupleBaseCell
+@property (nonatomic) UITextField *textField;
+@property (nonatomic) NSInteger maxInput;//最大输入限制，小于等于0表示不限制，默认为0
 @end
 
-@interface HText3ViewCell : HTupleBaseCell
-@property (nonatomic) UILabel *upLabel;
-@property (nonatomic) UILabel *downLabel;
-@end
--- QFProj/Classes/QFUtil/HTupleView/Cells/HTupleViewCell/HTupleViewCell.m
+++ QFProj/Classes/QFUtil/HTupleView/Cells/HTupleViewCell/HTupleViewCell.m
@@ -21,49 +21,6 @@
 }
 @end
 
-@implementation HLabelViewCell
-- (UILabel *)label {
-    if (!_label) {
-        _label = [UILabel new];
-        [self addSubview:_label];
-    }
-    return _label;
-}
-- (void)layoutContentView {
-    HLayoutTupleView(self.label)
-}
-@end
-
-@implementation HTextViewCell
-- (UITextView *)textView {
-    if (!_textView) {
-        _textView = [UITextView new];
-        [_textView setScrollEnabled:NO];
-        [_textView setUserInteractionEnabled:NO];
-        [self addSubview:_textView];
-    }
-    return _textView;
-}
-- (void)layoutContentView {
-    HLayoutTupleView(self.textView)
-}
-@end
-
-@implementation HTextFieldCell
-
-- (UITextField *)textField {
-    if (!_textField) {
-        _textField = UITextField.new;
-        [self addSubview:_textField];
-    }
-    return _textField;
-}
-
-- (void)layoutContentView {
-    HLayoutTupleView(self.textField)
-}
-@end
-
 @implementation HScrollViewCell
 - (UIScrollView *)scrollView {
     if (!_scrollView) {
@@ -77,71 +34,24 @@
 }
 @end
 
-@implementation HButtonViewCell
-- (HWebButtonView *)buttonView {
-    if (!_buttonView) {
-        _buttonView = [HWebButtonView new];
-        @weakify(self)
-        [_buttonView setPressed:^(id sender, id data) {
-            @strongify(self)
-            if (self.buttonViewBlock) {
-                self.buttonViewBlock(self.buttonView, self);
-            }
-        }];
-        [self addSubview:_buttonView];
+@implementation HTextViewCell
+- (UILabel *)label {
+    if (!_label) {
+        _label = [UILabel new];
+        [self addSubview:_label];
     }
-    return _buttonView;
+    return _label;
 }
 - (void)layoutContentView {
-    HLayoutTupleView(self.buttonView)
+    HLayoutTupleView(self.label)
 }
 @end
 
-@interface HImageViewCell ()
-@property (nonatomic) UITapGestureRecognizer *tapGesture;
-@end
-
-@implementation HImageViewCell
-- (HWebImageView *)imageView {
-    if (!_imageView) {
-        _imageView = [HWebImageView new];
-        [self addSubview:_imageView];
-    }
-    return _imageView;
-}
-- (UITapGestureRecognizer *)tapGesture {
-    if (!_tapGesture) {
-        _tapGesture = [[UITapGestureRecognizer alloc] init];
-        _tapGesture.numberOfTapsRequired = 1;
-        _tapGesture.numberOfTouchesRequired = 1;
-        [_tapGesture addTarget:self action:@selector(tapGestureAction)];
-    }
-    return _tapGesture;
-}
-- (void)tapGestureAction {
-    if (_imageViewBlock) {
-        _imageViewBlock(self.imageView, self);
-    }
-}
-- (void)setImageViewBlock:(HImageViewBlock)imageViewBlock {
-    if (_imageViewBlock != imageViewBlock) {
-        _imageViewBlock = nil;
-        _imageViewBlock = imageViewBlock;
-        if (!self.tapGesture.view) {
-            [self.imageView addGestureRecognizer:self.tapGesture];
-        }
-    }
-}
-- (void)layoutContentView {
-    HLayoutTupleView(self.imageView)
-}
-@end
-
-@interface HText2ViewCell ()
+@interface HTextViewCell2 ()
 @property (nonatomic) UIView *bgView;
 @end
 
-@implementation HText2ViewCell
+@implementation HTextViewCell2
 - (UIView *)bgView {
     if (!_bgView) {
         _bgView = [UIView new];
@@ -188,11 +98,11 @@
 }
 @end
 
-@interface HText3ViewCell ()
+@interface HTextViewCell3 ()
 @property (nonatomic) UIView *bgView;
 @end
 
-@implementation HText3ViewCell
+@implementation HTextViewCell3
 - (UIView *)bgView {
     if (!_bgView) {
         _bgView = [UIView new];
@@ -227,3 +137,148 @@
     self.downLabel.frame = frame;
 }
 @end
+
+@implementation HTextViewCell4
+- (UITextView *)textView {
+    if (!_textView) {
+        _textView = [UITextView new];
+        [_textView setScrollEnabled:NO];
+        [_textView setUserInteractionEnabled:NO];
+        [self addSubview:_textView];
+    }
+    return _textView;
+}
+- (void)layoutContentView {
+    HLayoutTupleView(self.textView)
+}
+@end
+
+@implementation HTextViewCell5
+- (YYLabel *)label {
+    if (!_label) {
+        _label = [YYLabel new];
+        [self addSubview:_label];
+    }
+    return _label;
+}
+- (void)layoutContentView {
+    HLayoutTupleView(self.label)
+}
+@end
+
+@implementation HButtonViewCell
+- (HWebButtonView *)button {
+    if (!_button) {
+        _button = [HWebButtonView new];
+        @weakify(self)
+        [_button setPressed:^(id sender, id data) {
+            @strongify(self)
+            if (self.buttonViewBlock) {
+                self.buttonViewBlock(self.button, self);
+            }
+        }];
+        [self addSubview:_button];
+    }
+    return _button;
+}
+- (void)layoutContentView {
+    HLayoutTupleView(self.button)
+}
+@end
+
+@interface HImageViewCell ()
+@property (nonatomic) UITapGestureRecognizer *tapGesture;
+@end
+
+@implementation HImageViewCell
+- (HWebImageView *)imageView {
+    if (!_imageView) {
+        _imageView = [HWebImageView new];
+        [self addSubview:_imageView];
+    }
+    return _imageView;
+}
+- (UITapGestureRecognizer *)tapGesture {
+    if (!_tapGesture) {
+        _tapGesture = [[UITapGestureRecognizer alloc] init];
+        _tapGesture.numberOfTapsRequired = 1;
+        _tapGesture.numberOfTouchesRequired = 1;
+        [_tapGesture addTarget:self action:@selector(tapGestureAction)];
+    }
+    return _tapGesture;
+}
+- (void)tapGestureAction {
+    if (_imageViewBlock) {
+        _imageViewBlock(self.imageView, self);
+    }
+}
+- (void)setImageViewBlock:(HImageViewBlock)imageViewBlock {
+    if (_imageViewBlock != imageViewBlock) {
+        _imageViewBlock = nil;
+        _imageViewBlock = imageViewBlock;
+        if (!self.tapGesture.view) {
+            [self.imageView addGestureRecognizer:self.tapGesture];
+        }
+    }
+}
+- (void)layoutContentView {
+    HLayoutTupleView(self.imageView)
+}
+@end
+
+@interface HTextFieldCell () <UITextFieldDelegate>
+
+@end
+
+@interface HTextFieldCell () <UITextFieldDelegate>
+
+@end
+
+@implementation HTextFieldCell
+
+- (UITextField *)textField {
+    if (!_textField) {
+        _textField = UITextField.new;
+        [_textField setDelegate:self];
+        [self addSubview:_textField];
+    }
+    return _textField;
+}
+
+- (void)layoutContentView {
+    HLayoutTupleView(self.textField)
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    if (self.maxInput > 0) {
+        NSInteger strLength = textField.text.length - range.length + string.length;
+        if (strLength > self.maxInput) {
+            NSString *tmpString = nil;
+            if (string.length > 1) {//复制字符串
+                NSCharacterSet *characterSet = [NSCharacterSet whitespaceCharacterSet];
+                string = [string stringByTrimmingCharactersInSet:characterSet];
+                tmpString = [textField.text stringByAppendingString:string];
+                //赋值
+                textField.text = [tmpString substringToIndex:self.maxInput];
+                //异步移动光标
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self cursorLocation:textField index:textField.text.length];
+                });
+            }else {//输入字符串
+                tmpString = [textField.text stringByAppendingString:string];
+                textField.text = [tmpString substringToIndex:self.maxInput];
+            }
+        }
+        return (strLength <= self.maxInput);
+    }
+    return YES;
+}
+//移动光标
+- (void)cursorLocation:(UITextField *)textField index:(NSInteger)index {
+    NSRange range = NSMakeRange(index, 0);
+    UITextPosition *start = [textField positionFromPosition:[textField beginningOfDocument] offset:range.location];
+    UITextPosition *end = [textField positionFromPosition:start offset:range.length];
+    [textField setSelectedTextRange:[textField textRangeFromPosition:start toPosition:end]];
+}
+@end
+
