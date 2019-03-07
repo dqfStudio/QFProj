ab31d1988a45ab70a994a2a6a54a2b63aa136b02
wind <897432098@qq.com>
2019-03-07 14:32:53 +0700

    modify some bugs

---
+++ QFProj/Classes/QFUtil/HPingTool/HPingTester.h
@@ -0,0 +1,33 @@
+//
+//  HPingTester.h
+//  BigVPN
+//
+//  Created by lingxuanfeng on 2017/5/11.
+//  Copyright © 2017年 lingxuanfeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SimplePing.h"
+
+typedef void(^HPingBlock)(NSString *hostName, NSTimeInterval time, NSError *error);
+
+@interface HPingTester : NSObject <SimplePingDelegate>
+
+@property (nonatomic) NSTimeInterval timeout;//default 1.5
+
+- (instancetype)init NS_UNAVAILABLE;
++ (instancetype)new NS_UNAVAILABLE;
+- (instancetype)initWithHostName:(NSString*)hostName NS_DESIGNATED_INITIALIZER;
+
+- (void)startPingWith:(HPingBlock)pingBlock;
+- (BOOL)isPinging;
+- (void)stopPing;
+
+@end
+
+@interface HPingItem : NSObject
+@property (nonatomic) uint16_t sequence;
+@end
+
+
+
---
+++ QFProj/Classes/QFUtil/HPingTool/HPingTester.m
@@ -0,0 +1,120 @@
+//
+//  HPingTester.m
+//  BigVPN
+//
+//  Created by lingxuanfeng on 2018/12/20.
+//  Copyright © 2018年 lingxuanfeng. All rights reserved.
+//
+
+#import "HPingTester.h"
+
+@interface HPingTester() {
+    NSTimer *_timer;
+    NSDate *_beginDate;
+    NSString *_hostName;
+}
+@property (nonatomic) SimplePing *simplePing;
+@property (nonatomic) NSMutableArray<HPingItem *> *pingItems;
+@property (nonatomic, copy) HPingBlock pingBlock;
+
+@end
+
+@implementation HPingTester
+
+- (instancetype)initWithHostName:(NSString*)hostName {
+    if (self = [super init]) {
+        _hostName = hostName;
+        self.simplePing = [[SimplePing alloc] initWithHostName:hostName];
+        self.simplePing.delegate = self;
+        self.simplePing.addressStyle = SimplePingAddressStyleAny;
+        self.timeout = 1.5;
+        self.pingItems = [NSMutableArray new];
+    }
+    return self;
+}
+
+- (void)startPingWith:(HPingBlock)pingBlock {
+    _pingBlock = pingBlock;
+    [self.simplePing start];
+}
+
+- (BOOL)isPinging {
+    return [_timer isValid];
+}
+
+- (void)stopPing {
+    [_timer invalidate];
+    _timer = nil;
+    [self.simplePing stop];
+}
+
+
+- (void)actionTimer {
+    if (_timer) {
+        [_timer invalidate];
+        _timer = nil;
+    }
+    _timer = [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(sendPingData) userInfo:nil repeats:YES];
+}
+
+- (void)sendPingData {
+    [self.simplePing sendPingWithData:nil];
+}
+
+
+#pragma mark - Ping Delegate
+- (void)simplePing:(SimplePing *)pinger didStartWithAddress:(NSData *)address {
+    [self actionTimer];
+}
+
+- (void)simplePing:(SimplePing *)pinger didFailWithError:(NSError *)error {
+    //NSLog(@"hostname:%@, ping失败--->%@", _hostName, error);
+    if (self.pingBlock) {
+        self.pingBlock(_hostName, 0, error);
+    }
+}
+
+- (void)simplePing:(SimplePing *)pinger didSendPacket:(NSData *)packet sequenceNumber:(uint16_t)sequenceNumber {
+    HPingItem *item = [HPingItem new];
+    item.sequence = sequenceNumber;
+    [self.pingItems addObject:item];
+    
+    _beginDate = [NSDate date];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(self.timeout * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if ([self.pingItems containsObject:item]) {
+            //NSLog(@"hostname:%@, 超时---->", _hostName);
+            [self.pingItems removeObject:item];
+            if (self.pingBlock) {
+                self.pingBlock(self->_hostName, 0, [NSError errorWithDomain:NSURLErrorDomain code:111 userInfo:nil]);
+            }
+        }
+    });
+}
+- (void)simplePing:(SimplePing *)pinger didFailToSendPacket:(NSData *)packet sequenceNumber:(uint16_t)sequenceNumber error:(NSError *)error {
+    //NSLog(@"hostname:%@, 发包失败--->%@", _hostName, error);
+    if (self.pingBlock) {
+        self.pingBlock(_hostName, 0, error);
+    }
+}
+
+- (void)simplePing:(SimplePing *)pinger didReceivePingResponsePacket:(NSData *)packet sequenceNumber:(uint16_t)sequenceNumber {
+    float delayTime = [[NSDate date] timeIntervalSinceDate:_beginDate] * 1000;
+    [self.pingItems enumerateObjectsUsingBlock:^(HPingItem * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (obj.sequence == sequenceNumber) {
+            [self.pingItems removeObject:obj];
+        }
+    }];
+    if (self.pingBlock) {
+        self.pingBlock(_hostName, delayTime, nil);
+    }
+}
+
+- (void)simplePing:(SimplePing *)pinger didReceiveUnexpectedPacket:(NSData *)packet {
+    
+}
+
+@end
+
+@implementation HPingItem
+
+@end
