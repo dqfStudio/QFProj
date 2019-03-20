e801551c8f6397aab003b8393c5fe579bda9004a
wind <897432098@qq.com>
2019-03-20 23:29:45 +0700

    modify some files

--- QFProj/Classes/QFUtil/HSafeUtil/NSObject+HForwarding.m
+++ QFProj/Classes/QFUtil/HSafeUtil/NSObject+HForwarding.m
@@ -9,14 +9,14 @@
 #import "NSObject+HForwarding.h"
 
 @implementation NSObject (HForwarding)
-
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
 //方法签名
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
     return [NSMethodSignature signatureWithObjCTypes:"v@:@"];
 }
-
 - (void)forwardInvocation:(NSInvocation *)anInvocation {
     
 }
-
+#pragma clang diagnostic pop
 @end
