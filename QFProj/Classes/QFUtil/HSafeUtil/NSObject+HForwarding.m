d7d83a111d5cba9310a22416de7c9b6708fef446
wind <897432098@qq.com>
2019-03-22 15:14:28 +0700

    modify some files

--- QFProj/Classes/QFUtil/HSafeUtil/NSObject+HForwarding.m
+++ QFProj/Classes/QFUtil/HSafeUtil/NSObject+HForwarding.m
@@ -15,6 +15,9 @@
 #define KMethodSignature \
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {\
     NSString *selectorString = NSStringFromSelector(aSelector);\
+    if ([self isKindOfClass:NSClassFromString(@"UITextInputController")] || [NSStringFromClass(self.class)\ hasPrefix:@"UIKeyboard"] || [selectorString isEqualToString:@"dealloc"]) {\
+        return nil;\
+    }\
     NSUInteger count = [[selectorString componentsSeparatedByString:@":"] count] - 1;\
     NSString *string = @":@:";\
     for (int i=0; i<count; i++) {\
@@ -26,86 +29,84 @@
 
 
 @implementation NSObject (HForwarding)
-//#pragma clang diagnostic push
-//#pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
-//- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
-//    NSString *selectorString = NSStringFromSelector(aSelector);
-//    NSUInteger count = [[selectorString componentsSeparatedByString:@":"] count] - 1;
-//    //NSString *string = @"v@:";
-//    //NSString *string = @"@@:";
-//    NSString *string = @":@:";
-//    for (int i=0; i<count; i++) {
-//        string = [string stringByAppendingString:@":"];
-//        //string = [string stringByAppendingString:@"@"];
-//    }
-//    return [NSMethodSignature signatureWithObjCTypes:[string UTF8String]];
-//}
-//- (void)forwardInvocation:(NSInvocation *)anInvocation {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
+    NSString *selectorString = NSStringFromSelector(aSelector);
+    if ([self isKindOfClass:NSClassFromString(@"UITextInputController")] || [NSStringFromClass(self.class) hasPrefix:@"UIKeyboard"] || [selectorString isEqualToString:@"dealloc"]) {
+        return nil;
+    }
+    NSUInteger count = [[selectorString componentsSeparatedByString:@":"] count] - 1;
+    NSString *string = @":@:";
+    for (int i=0; i<count; i++) {
+        string = [string stringByAppendingString:@":"];
+    }
+    return [NSMethodSignature signatureWithObjCTypes:[string UTF8String]];
+}
+- (void)forwardInvocation:(NSInvocation *)anInvocation {}
+#pragma clang diagnostic pop
+@end
+
+//@implementation NSString (HForwarding)
+//KMethodSignature
+//@end
 //
-//}
-//#pragma clang diagnostic pop
-@end
-
-@implementation NSString (HForwarding)
-KMethodSignature
-@end
-
-@implementation NSValue (HForwarding)
-KMethodSignature
-@end
-
-@implementation UIResponder (HForwarding)
-KMethodSignature
-@end
-
-@implementation NSSet (HForwarding)
-KMethodSignature
-@end
-
-@implementation NSArray (HForwarding)
-KMethodSignature
-@end
-
-@implementation NSDictionary (HForwarding)
-KMethodSignature
-@end
-
-@implementation NSTimer (HForwarding)
-KMethodSignature
-@end
-
-@implementation NSNull (HForwarding)
-KMethodSignature
-@end
-
-@implementation NSError (HForwarding)
-KMethodSignature
-@end
-
-@implementation NSData (HForwarding)
-KMethodSignature
-@end
-
-@implementation NSDate (HForwarding)
-KMethodSignature
-@end
-
-@implementation UIFont (HForwarding)
-KMethodSignature
-@end
-
-@implementation UIImage (HForwarding)
-KMethodSignature
-@end
-
-@implementation UIScreen (HForwarding)
-KMethodSignature
-@end
-
-@implementation UIDevice (HForwarding)
-KMethodSignature
-@end
-
-@implementation UIColor (HForwarding)
-KMethodSignature
-@end
+//@implementation NSValue (HForwarding)
+//KMethodSignature
+//@end
+//
+//@implementation UIResponder (HForwarding)
+//KMethodSignature
+//@end
+//
+//@implementation NSSet (HForwarding)
+//KMethodSignature
+//@end
+//
+//@implementation NSArray (HForwarding)
+//KMethodSignature
+//@end
+//
+//@implementation NSDictionary (HForwarding)
+//KMethodSignature
+//@end
+//
+//@implementation NSTimer (HForwarding)
+//KMethodSignature
+//@end
+//
+//@implementation NSNull (HForwarding)
+//KMethodSignature
+//@end
+//
+//@implementation NSError (HForwarding)
+//KMethodSignature
+//@end
+//
+//@implementation NSData (HForwarding)
+//KMethodSignature
+//@end
+//
+//@implementation NSDate (HForwarding)
+//KMethodSignature
+//@end
+//
+//@implementation UIFont (HForwarding)
+//KMethodSignature
+//@end
+//
+//@implementation UIImage (HForwarding)
+//KMethodSignature
+//@end
+//
+//@implementation UIScreen (HForwarding)
+//KMethodSignature
+//@end
+//
+//@implementation UIDevice (HForwarding)
+//KMethodSignature
+//@end
+//
+//@implementation UIColor (HForwarding)
+//KMethodSignature
+//@end
