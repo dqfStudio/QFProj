584bd9d98ad76d5d088553259fbf7ae412710c23
wind <897432098@qq.com>
2019-04-10 16:23:17 +0700

    modify some files

--- QFProj/Classes/QFUtil/HTable/Cells/HTableViewCell.h
+++ QFProj/Classes/QFUtil/HTable/Cells/HTableViewCell.h
@@ -8,15 +8,6 @@
 
 #import "HBaseCell.h"
 
-typedef struct HCellEdgeInsets {
-    CGFloat width, height, left, right;
-} HCellEdgeInsets;
-
-CG_EXTERN HCellEdgeInsets HCellEdgeInsetsMake(CGFloat width, CGFloat height, CGFloat left, CGFloat right);
-CG_EXTERN bool HCellEdgeEqualToEdge(HCellEdgeInsets edge1, HCellEdgeInsets edge2);
-
 @interface HTableViewCell : HBaseCell
-@property (nonatomic) UIImageView *leftImageView; //代替系统的imageView控件
-@property (nonatomic) HCellEdgeInsets leftImageEdgeInsets; //图片大小和左右边距，上下边距自动计算且居中
-@property (nonatomic) CGFloat leftImageViewCornerRadius;//leftImageView控件圆角弧度
+
 @end
