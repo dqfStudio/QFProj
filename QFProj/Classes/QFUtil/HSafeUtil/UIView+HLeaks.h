9c98307aeb7695ad3b42752df867cf1573d8a76d
wind <897432098@qq.com>
2019-07-16 16:07:55 +0700

    modify some files

---
+++ QFProj/Classes/QFUtil/HSafeUtil/UIView+HLeaks.h
@@ -0,0 +1,13 @@
+//
+//  UIView+HLeaks.h
+//  QFProj
+//
+//  Created by wind on 2019/7/16.
+//  Copyright © 2019 dqfStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (HLeaks)
+
+@end
---
+++ QFProj/Classes/QFUtil/HSafeUtil/UIView+HLeaks.m
@@ -0,0 +1,50 @@
+
+//
+//  UIView+HLeaks.m
+//  QFProj
+//
+//  Created by wind on 2019/7/16.
+//  Copyright © 2019 dqfStudio. All rights reserved.
+//
+
+#import "UIView+HLeaks.h"
+#import <objc/message.h>
+#import "NSObject+HSwizzleUtil.h"
+
+@implementation UIView (HLeaks)
+//+ (void)load {
+//    static dispatch_once_t onceToken;
+//    dispatch_once(&onceToken, ^{
+//        [[self class] methodSwizzleWithOrigSEL:@selector(willMoveToSuperview:) overrideSEL:@selector(leaks_willMoveToSuperview:)];
+//    });
+//}
+- (void)leaks_willMoveToSuperview:(UIView *)newView {
+    if ([self isCustomClass]) {
+        if (!newView) {
+            [self willDealloc];
+        }
+    }
+}
+//即将调用dealloc
+- (void)willDealloc {
+    __weak typeof(self) weakSelf = self;
+    //延时3s，留足释放内存时间
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        __strong typeof(self) strongSelf = weakSelf;
+        //这个的原理就是通过给nil发送方法，如何不为空就执行，为nil不执行
+        [strongSelf isNotDealloc];
+    });
+}
+//打印没有释放的view
+- (void)isNotDealloc {
+    NSLog(@"🍎🍎🍎🍎🍎🍎🍎%@ is not dealloc🍎🍎🍎🍎🍎🍎🍎",NSStringFromClass([self class]));
+}
+- (BOOL)isCustomClass {
+    NSBundle *mainB = [NSBundle bundleForClass:[self class]];
+    if (mainB == [NSBundle mainBundle]) {
+        return YES; //自定义类
+    }else {
+        return NO; //系统类
+    }
+}
+@end
