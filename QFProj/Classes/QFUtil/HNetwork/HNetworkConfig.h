5f6ea384dc8ecf35a500af4c17bd444bf7850a8d
wind <897432098@qq.com>
2019-07-19 20:16:45 +0700

    modify some files

---
+++ QFProj/Classes/QFUtil/HNetwork/HNetworkConfig.h
@@ -0,0 +1,17 @@
+//
+//  HNetworkConfig.h
+//  QFProj
+//
+//  Created by wind on 2019/7/19.
+//  Copyright © 2019 dqfStudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HNetworkConfig : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ QFProj/Classes/QFUtil/HNetwork/HNetworkConfig.m
@@ -0,0 +1,22 @@
+//
+//  HNetworkConfig.m
+//  QFProj
+//
+//  Created by wind on 2019/7/19.
+//  Copyright © 2019 dqfStudio. All rights reserved.
+//
+
+#import "HNetworkConfig.h"
+
+@implementation HNetworkConfig
+
+////https 允许无效证书访问
+//AFSecurityPolicy *security = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModeNone];
+//[security setValidatesDomainName:NO];
+//security.allowInvalidCertificates = YES;
+//[YTKNetworkConfig sharedConfig].securityPolicy = security;
+
+
+//set base url
+//[[YTKNetworkConfig sharedConfig] setBaseUrl:HEADBASEINURL];
+@end
