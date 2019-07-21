c69dc133a16a2b6b0840984cfe48211d62aabb79
wind <897432098@qq.com>
2019-07-21 11:56:43 +0700

    modify some files

---
+++ QFProj/Classes/QFUtil/HMonitor/HMonitor.h
@@ -0,0 +1,20 @@
+//
+//  HMonitor.h
+//  RunLoopMonitor
+//
+//  Created by Eleven on 2018/11/17.
+//  Copyright © 2018 Eleven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HMonitor : NSObject
++ (instancetype)sharedInstance;
+- (void)startMonitor;
+- (void)endMonitor;
+- (void)printLogTrace;
+@end
+
+NS_ASSUME_NONNULL_END
