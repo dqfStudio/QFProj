a715b40f5f9dea4ea3016bdbc22c686323557804
wind <897432098@qq.com>
2019-08-19 09:23:54 +0700

    modify some files

--- QFProj/Classes/QFUtil/NSBlockChain/NSBlockChain.h
+++ QFProj/Classes/QFUtil/NSBlockChain/NSBlockChain.h
@@ -8,16 +8,16 @@
 
 #import <Foundation/Foundation.h>
 
-@interface NSBlock : NSObject
-@property (nonatomic) NSInteger index;
-@property (nonatomic) NSString  *dateCreated;
-@property (nonatomic) NSString  *previousHash;
-@property (nonatomic) NSString  *hashString;
-@property (nonatomic) NSInteger nonce;
-@end
-
-@interface NSBlockChain : NSObject
-- (id)initWithBlock:(NSBlock *)genesisBlock;
-- (void)addBlock:(NSBlock *)block;
-- (NSArray *)allBlocks;
-@end
+//@interface NSBlock : NSObject
+//@property (nonatomic) NSInteger index;
+//@property (nonatomic) NSString  *dateCreated;
+//@property (nonatomic) NSString  *previousHash;
+//@property (nonatomic) NSString  *hashString;
+//@property (nonatomic) NSInteger nonce;
+//@end
+//
+//@interface NSBlockChain : NSObject
+//- (id)initWithBlock:(NSBlock *)genesisBlock;
+//- (void)addBlock:(NSBlock *)block;
+//- (NSArray *)allBlocks;
+//@end
