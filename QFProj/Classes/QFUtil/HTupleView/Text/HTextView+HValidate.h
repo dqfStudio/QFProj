01114a205c9a15b62a02478480455ac99495b70e
wind <897432098@qq.com>
2019-09-03 11:50:47 +0700

    modify some files

---
+++ QFProj/Classes/QFUtil/HTupleView/Text/HTextView+HValidate.h
@@ -0,0 +1,48 @@
+//
+//  HTextView+HValidate.h
+//  QFProj
+//
+//  Created by wind on 2019/9/3.
+//  Copyright © 2019 dqfStudio. All rights reserved.
+//
+
+#import "HTextView.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HTextView (HValidate)
+
+// 须是字母与数字的组合，长度6-11位
+@property (nonatomic, readonly) BOOL isValidatedUserName; //是否有效的用户名
+// 字母、数字或其组合，长度6-12位
+@property (nonatomic, readonly) BOOL isValidatedPassword; //是否有效的密码
+
+//@property (nonatomic, readonly, getter=isEmpty) BOOL empty; //是否为空
+@property (nonatomic, readonly) BOOL isEmpty; //是否为空
+
+@property (nonatomic, readonly) BOOL isOnlyAlpha; //纯字母
+@property (nonatomic, readonly) BOOL isOnlyNumeric; //纯数字
+@property (nonatomic, readonly) BOOL isAlphaNumeric; //须是字母与数字的组合，默认验证2-10000位
+@property (nonatomic, readonly) BOOL isAlphaOrNumeric; //字母、数字或两者的组合
+
+@property (nonatomic, readonly) BOOL isValidatedEmial; //是否有效的邮箱
+@property (nonatomic, readonly) BOOL isValidatedVCode; //是否有效的验证码
+@property (nonatomic, readonly) BOOL isValidatedMobile; //是否有效的手机号
+@property (nonatomic, readonly) BOOL isValidatedIDCard; //是否有效的身份证号
+
+@property (nonatomic, readonly) BOOL isValidatedCarNo; //是否有效的车牌号
+@property (nonatomic, readonly) BOOL isValidatedCarType; //是否有效的车型
+
+@property (nonatomic, readonly) BOOL isOnlyChinese; //纯中文
+// 微信号校验 可以使用6—20个字母、数字、下划线和减号，必须以字母开头
+@property (nonatomic, readonly) BOOL isValidatedWechat; //是否有效的微信号
+@property (nonatomic, readonly) BOOL isValidatedBankCard; //是否有效的银行卡账号
+
+@property (nonatomic, readonly) BOOL isContainIllegalCharacters; //是否包含特殊字符
+
+- (BOOL (^)(NSInteger length))isEqualto; //判断内容长度是否等于某个值
+- (BOOL (^)(NSInteger start, NSInteger end))isBetween;//判断内容长度是否在某两个值之间
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ QFProj/Classes/QFUtil/HTupleView/Text/HTextView+HValidate.m
@@ -0,0 +1,131 @@
+//
+//  HTextView+HValidate.m
+//  QFProj
+//
+//  Created by wind on 2019/9/3.
+//  Copyright © 2019 dqfStudio. All rights reserved.
+//
+
+#import "HTextView+HValidate.h"
+
+@implementation HTextView (HValidate)
+
+- (BOOL)isValidatedUserName {
+    NSString *regex = @"^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,11}$";
+    return [self isValidateWithRegex:regex];
+}
+- (BOOL)isValidatedPassword {
+    NSString *regex = @"[a-zA-Z0-9]{6,12}$";
+    return [self isValidateWithRegex:regex];
+}
+
+
+- (BOOL)isEmpty {
+    return (self.text.length == 0);
+}
+- (BOOL)isOnlyAlpha {
+    NSString *regex = @"[a-zA-Z]*";
+    return [self isValidateWithRegex:regex];
+}
+- (BOOL)isOnlyNumeric {
+    NSString *regex = @"[0-9]*";
+    return [self isValidateWithRegex:regex];
+}
+- (BOOL)isAlphaNumeric {
+    NSString *regex = @"^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{2,10000}$";
+    return [self isValidateWithRegex:regex];
+}
+- (BOOL)isAlphaOrNumeric {
+    NSString *regex = @"^[A-Za-z0-9]+$";
+    return [self isValidateWithRegex:regex];
+}
+
+
+
+- (BOOL)isValidatedEmial {
+    NSString *regex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    return [self isValidateWithRegex:regex];
+}
+- (BOOL)isValidatedVCode {
+    NSString *regex = @"[0-9]{4,6}";
+    return [self isValidateWithRegex:regex];
+}
+- (BOOL)isValidatedMobile {
+    /**
+     * 手机号码:
+     * 13[0-9], 14[5,7], 15[0, 1, 2, 3, 5, 6, 7, 8, 9], 16[6], 17[5, 6, 7, 8], 18[0-9], 170[0-9], 19[89]
+     * 移动号段: 134,135,136,137,138,139,150,151,152,157,158,159,182,183,184,187,188,147,178,1705,198
+     * 联通号段: 130,131,132,155,156,185,186,145,175,176,1709,166
+     * 电信号段: 133,153,180,181,189,177,1700,199
+     */
+    NSString *MOBILE = @"^1(3[0-9]|4[57]|5[0-35-9]|6[6]|7[05-8]|8[0-9]|9[89])\\d{8}$";
+    
+    NSString *CM = @"(^1(3[4-9]|4[7]|5[0-27-9]|7[8]|8[2-478]|9[8])\\d{8}$)|(^1705\\d{7}$)";
+    
+    NSString *CU = @"(^1(3[0-2]|4[5]|5[56]|66|7[56]|8[56])\\d{8}$)|(^1709\\d{7}$)";
+    
+    NSString *CT = @"(^1(33|53|77|8[019]|99)\\d{8}$)|(^1700\\d{7}$)";
+    
+    if([self isValidateWithRegex:MOBILE] || [self isValidateWithRegex:CM] || [self isValidateWithRegex:CU] || [self isValidateWithRegex:CT]) {
+        return YES;
+    }else {
+        return NO;
+    }
+}
+- (BOOL)isValidatedIDCard {
+    NSString *regex = @"^(\\d{14}|\\d{17})(\\d|[xX])$";
+    return [self isValidateWithRegex:regex];
+}
+
+
+
+- (BOOL)isValidatedCarNo {
+    NSString *regex = @"^[\u4e00-\u9fa5]{1}[a-zA-Z]{1}[a-zA-Z_0-9]{4}[a-zA-Z_0-9_\u4e00-\u9fa5]$";
+    return [self isValidateWithRegex:regex];
+}
+- (BOOL)isValidatedCarType {
+    NSString *regex = @"^[\u4E00-\u9FFF]+$";
+    return [self isValidateWithRegex:regex];
+}
+
+
+
+- (BOOL)isOnlyChinese {
+    NSString *regex = @"[\u4e00-\u9fa5]+";
+    return [self isValidateWithRegex:regex];
+}
+- (BOOL)isValidatedWechat {
+    NSString *regex = @"^[a-zA-Z]([-_a-zA-Z0-9]{5,19})+$";
+    return [self isValidateWithRegex:regex];
+}
+- (BOOL)isValidatedBankCard {
+    NSString *regex = @"[1-9]([0-9]{13,19})";
+    return [self isValidateWithRegex:regex];
+}
+- (BOOL)isContainIllegalCharacters {
+    NSString *regex = @"^[A-Za-z0-9\\u4e00-\u9fa5]+$";
+    return ![self isValidateWithRegex:regex];
+}
+
+
+
+- (BOOL (^)(NSInteger length))isEqualto {
+    return ^BOOL (NSInteger length) {
+        if (self.text.length == length) {
+            return YES;
+        }
+        return NO;
+    };
+}
+- (BOOL (^)(NSInteger start, NSInteger end))isBetween {
+    return ^BOOL (NSInteger start, NSInteger end) {
+        if (self.text.length >= start && self.text.length <= end) {
+            return YES;
+        }
+        return NO;
+    };
+}
+- (BOOL)isValidateWithRegex:(NSString *)regex {
+    return [[NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex] evaluateWithObject:self.text];
+}
+@end
