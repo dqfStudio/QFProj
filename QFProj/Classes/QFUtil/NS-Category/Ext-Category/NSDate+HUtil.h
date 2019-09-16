a6939f791dd7c63be6a87932970d1af51b176250
wind <897432098@qq.com>
2019-09-16 20:23:16 +0700

    modify some files

--- QFProj/Classes/QFUtil/NS-Category/Ext-Category/NSDate+HUtil.h
+++ QFProj/Classes/QFUtil/NS-Category/Ext-Category/NSDate+HUtil.h
@@ -8,8 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSInteger, NSDateFormat) {
+    NSDateFormatDefault = 0, //yyyy-MM-dd HH:mm:ss
+    NSDateFormatValue1, //yyyy-MM-dd
+    NSDateFormatValue2 //yyyy年MM月dd日
+};
+
 @interface NSDate (HUtil)
 + (void)startTime;
 + (void)endTime;
 + (void)time:(void(^)(void))callback;
+
++ (NSString *)dateWithFormat:(NSDateFormat)format;
++ (NSDate *)dateWithString:(NSString*)aString format:(NSDateFormat)format;
++ (NSString *)weekdayFromDate:(NSDate *)date;
++ (NSString *)pastWithDays:(NSUInteger)days format:(NSDateFormat)format;
++ (NSString *)pastWithDate:(NSDate *)date Days:(NSUInteger)days format:(NSDateFormat)format;
++ (NSDate *)pastWithDays:(NSUInteger)days;
++ (NSDate *)pastWithDate:(NSDate *)date days:(NSUInteger)days;
 @end
--- QFProj/Classes/QFUtil/NS-Category/Ext-Category/NSDate+HUtil.m
+++ QFProj/Classes/QFUtil/NS-Category/Ext-Category/NSDate+HUtil.m
@@ -27,4 +27,63 @@ static const NSDate *startDate = nil;
         NSLog(@"block time: %f", -[startDate timeIntervalSinceNow]);
     }
 }
+
++ (NSString *)dateWithFormat:(NSDateFormat)format {
+    NSString *formatString = [self stringWithFormat:format];
+    NSDateFormatter *formatter = NSDateFormatter.new;
+    [formatter setDateFormat:formatString];
+    return [formatter stringFromDate:[NSDate date]];
+}
++ (NSDate *)dateWithString:(NSString*)aString format:(NSDateFormat)format {
+    if (!aString || aString.length == 0) return nil;
+    NSString *formatString = [self stringWithFormat:format];
+    NSDateFormatter *formatter = NSDateFormatter.new;
+    [formatter setDateFormat:formatString];
+    return [formatter dateFromString:aString];
+}
++ (NSString *)weekdayFromDate:(NSDate *)date {
+    NSArray *weekdays = [NSArray arrayWithObjects: [NSNull null], @"星期天", @"星期一", @"星期二", @"星期三", @"星期四", @"星期五", @"星期六", nil];
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierChinese];
+    NSTimeZone *timeZone = [[NSTimeZone alloc] initWithName:@"Asia/Shanghai"];
+    [calendar setTimeZone: timeZone];
+    NSCalendarUnit calendarUnit = NSCalendarUnitWeekday;
+    NSDateComponents *theComponents = [calendar components:calendarUnit fromDate:date];
+    return [weekdays objectAtIndex:theComponents.weekday];
+}
++ (NSString *)pastWithDays:(NSUInteger)days format:(NSDateFormat)format {
+    return [self pastWithDate:NSDate.date Days:days format:format];
+}
++ (NSString *)pastWithDate:(NSDate *)date Days:(NSUInteger)days format:(NSDateFormat)format {
+    NSString *formatString = [self stringWithFormat:format];
+    NSDateFormatter *formatter = NSDateFormatter.new;
+    [formatter setDateFormat:formatString];
+    NSDate *pastDay = [self pastWithDate:date days:days];
+    return [formatter stringFromDate:pastDay];
+}
++ (NSDate *)pastWithDays:(NSUInteger)days {
+    return [self pastWithDate:NSDate.date days:days];
+}
++ (NSDate *)pastWithDate:(NSDate *)date days:(NSUInteger)days {
+    if (!date || days < 0) return nil;
+    NSTimeInterval time = days * 24 * 60 * 60;
+    return [date dateByAddingTimeInterval:-time];
+}
++ (NSString *)stringWithFormat:(NSDateFormat)format {
+    NSString *formatString = nil;
+    switch (format) {
+        case NSDateFormatDefault:
+            formatString = @"yyyy-MM-dd HH:mm:ss";
+            break;
+        case NSDateFormatValue1:
+            formatString = @"yyyy-MM-dd";
+            break;
+        case NSDateFormatValue2:
+            formatString = @"yyyy年MM月dd日";
+            break;
+        default:
+            formatString = @"yyyy-MM-dd HH:mm:ss";
+            break;
+    }
+    return formatString;
+}
 @end
