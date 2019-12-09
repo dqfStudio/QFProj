faff531994868175114889be6c472df19f296505
wind <897432098@qq.com>
2019-12-09 20:14:52 +0700

    modify some files

--- QFProj/Classes/QFUtil/HTupleView/HSelector/NSObject+HSelector.m
+++ QFProj/Classes/QFUtil/HTupleView/HSelector/NSObject+HSelector.m
@@ -10,71 +10,14 @@
 
 @implementation NSObject (HSelector)
 
-- (BOOL)respondsToSelector:(SEL)aSelector withPre:(NSString *)pre {
-    NSString *selectorString = [NSString stringWithFormat:@"%@%@",pre,NSStringFromSelector(aSelector)];
-    SEL selector = NSSelectorFromString(selectorString);
-    return [self respondsToSelector:selector];
-}
-
 #pragma --mark 执行实例方法
 
 - (id)performSelector:(SEL)aSelector withObjects:(NSArray *)objects {
-    NSMethodSignature *signature = [[self class] instanceMethodSignatureForSelector:aSelector];
-    return [[self class] performSelector:aSelector withSignature:signature withObjects:objects withMethodArgments:nil];
-}
-
-- (id)performSelector:(SEL)aSelector withMethodArgments:(void *)firstParameter, ... {
-    NSMethodSignature *signature = [[self class] instanceMethodSignatureForSelector:aSelector];
-    return [[self class] performSelector:aSelector withSignature:signature withObjects:nil withMethodArgments:firstParameter];
-}
-
-- (id)performSelector:(SEL)aSelector withPre:(NSString *)pre withMethodArgments:(void *)firstParameter, ... {
-    NSString *selectorString = [NSString stringWithFormat:@"%@%@",pre,NSStringFromSelector(aSelector)];
-    SEL selector = NSSelectorFromString(selectorString);
-    NSMethodSignature *signature = [[self class] instanceMethodSignatureForSelector:selector];
-    return [[self class] performSelector:aSelector withSignature:signature withObjects:nil withMethodArgments:firstParameter];
-}
-
-#pragma --mark 执行类方法
-
-+ (id)performClassSelector:(SEL)aSelector withObjects:(NSArray *)objects {
-    NSMethodSignature *signature = [[self class] methodSignatureForSelector:aSelector];
-    return [self performSelector:aSelector withSignature:signature withObjects:objects withMethodArgments:nil];
-}
-
-- (id)performClassSelector:(SEL)aSelector withObjects:(NSArray *)objects {
-    return [[self class] performClassSelector:aSelector withObjects:objects];
-}
-
-
-
-
-+ (id)performClassSelector:(SEL)aSelector withMethodArgments:(void *)firstParameter, ... {
-    NSMethodSignature *signature = [[self class] methodSignatureForSelector:aSelector];
-    return [self performSelector:aSelector withSignature:signature withObjects:nil withMethodArgments:firstParameter];
-}
-
-- (id)performClassSelector:(SEL)aSelector withMethodArgments:(void *)firstParameter, ... {
-    return [[self class] performClassSelector:aSelector withMethodArgments:firstParameter];
-}
-
-
-
-+ (id)performClassSelector:(SEL)aSelector withPre:(NSString *)pre withMethodArgments:(void *)firstParameter, ... {
-    NSString *selectorString = [NSString stringWithFormat:@"%@%@",pre,NSStringFromSelector(aSelector)];
-    SEL selector = NSSelectorFromString(selectorString);
-    return [self performClassSelector:selector withMethodArgments:firstParameter];
-}
-
-- (id)performClassSelector:(SEL)aSelector withPre:(NSString *)pre withMethodArgments:(void *)firstParameter, ... {
-    return [self performSelector:aSelector withPre:pre withMethodArgments:firstParameter];
-}
-
-
-
-+ (id)performSelector:(SEL)aSelector withSignature:(NSMethodSignature *)signature withObjects:(NSArray *)objects withMethodArgments:(void *)firstParameter, ... {
     
-    //1、判断传入的方法是否存在
+    //1、创建签名对象
+    NSMethodSignature *signature = [[self class] instanceMethodSignatureForSelector:aSelector];
+    
+    //2、判断传入的方法是否存在
     if (signature==nil) {
         //传入的方法不存在 就抛异常
         NSString *info = [NSString stringWithFormat:@"-[%@ %@]:unrecognized selector sent to instance",[self class],NSStringFromSelector(aSelector)];
@@ -82,43 +25,43 @@
         return nil;
     }
     
-    //2、创建NSInvocation对象
+    //3、、创建NSInvocation对象
     NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
     
-    //3、保存方法所属的对象
+    //4、保存方法所属的对象
     invocation.target = self;
     invocation.selector = aSelector;
     
-    //4、设置参数
-    if (objects.count > 0) {
-        NSInteger arguments =signature.numberOfArguments-2;
-        NSUInteger objectsCount = objects.count;
-        NSInteger count = MIN(arguments, objectsCount);
-        for (int i = 0; i<count; i++) {
-            NSObject*obj = objects[i];
-            //处理参数是NULL类型的情况
-            if ([obj isKindOfClass:[NSNull class]]) {
-                obj = nil;
-            }
-            [invocation setArgument:&obj atIndex:i+2];
+    //5、设置参数
+    /*
+     当前如果直接遍历参数数组来设置参数
+     如果参数数组元素多余参数个数，那么就会报错
+     */
+    NSInteger arguments =signature.numberOfArguments-2;
+    
+    /*
+     如果直接遍历参数的个数，会存在问题
+     如果参数的个数大于了参数值的个数，那么数组会越界
+     */
+    /*
+     谁少就遍历谁
+     */
+    NSUInteger objectsCount = objects.count;
+    NSInteger count = MIN(arguments, objectsCount);
+    for (int i = 0; i<count; i++) {
+        NSObject*obj = objects[i];
+        //处理参数是NULL类型的情况
+        if ([obj isKindOfClass:[NSNull class]]) {
+            obj = nil;
         }
-    }else {
-        if (signature.numberOfArguments > 2) {
-            [invocation setArgument:firstParameter atIndex:2];
-            va_list arg_ptr;
-            va_start(arg_ptr, firstParameter);
-            for (NSUInteger i = 3; i < signature.numberOfArguments; i++) {
-                void * parameter = va_arg(arg_ptr, void *);
-                [invocation setArgument:parameter atIndex:i];
-            }
-            va_end(arg_ptr);
-        }
+        [invocation setArgument:&obj atIndex:i+2];
+        
     }
     
-    //5、调用NSinvocation对象
+    //6、调用NSinvocation对象
     [invocation invoke];
     
-    //6、获取返回值
+    //7、获取返回值
 
     //获得返回值类型
     const char *returnType = signature.methodReturnType;
@@ -145,11 +88,86 @@
         if(!strcmp(returnType, @encode(BOOL)) ) {
             returnValue = [NSNumber numberWithBool:*((BOOL*)buffer)];
         }
-        else if(!strcmp(returnType, @encode(float)) ){
-            returnValue = [NSNumber numberWithFloat:*((float*)buffer)];
+        else if(!strcmp(returnType, @encode(NSInteger)) ){
+            returnValue = [NSNumber numberWithInteger:*((NSInteger*)buffer)];
         }
-        else if(!strcmp(returnType, @encode(double)) ){
-            returnValue = [NSNumber numberWithDouble:*((double*)buffer)];
+        else if(!strcmp(returnType, @encode(NSUInteger)) ){
+            returnValue = [NSNumber numberWithUnsignedInteger:*((NSUInteger*)buffer)];
+        }
+        else if(!strcmp(returnType, @encode(int))) {
+            returnValue = [NSNumber numberWithInt:*((int*)buffer)];
+        }
+        else if(!strcmp(returnType, @encode(char))) {
+            returnValue = [NSNumber numberWithChar:*((char*)buffer)];
+        }
+        else {
+            returnValue = [NSValue valueWithBytes:buffer objCType:returnType];
+        }
+    }
+    return returnValue;
+}
+
+- (id)performSelector:(SEL)aSelector withMethodArgments:(void *)firstParameter, ... {
+    
+    //1、创建签名对象
+    NSMethodSignature *signature = [[self class] instanceMethodSignatureForSelector:aSelector];
+    
+    //2、判断传入的方法是否存在
+    if (signature==nil) {
+        //传入的方法不存在 就抛异常
+        NSString *info = [NSString stringWithFormat:@"-[%@ %@]:unrecognized selector sent to instance",[self class],NSStringFromSelector(aSelector)];
+        @throw [[NSException alloc] initWithName:@"此方法没有" reason:info userInfo:nil];
+        return nil;
+    }
+    
+    //3、、创建NSInvocation对象
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+    
+    //4、保存方法所属的对象
+    invocation.target = self;
+    invocation.selector = aSelector;
+    
+    //5、设置参数
+    if (signature.numberOfArguments > 2) {
+        [invocation setArgument:firstParameter atIndex:2];
+        va_list arg_ptr;
+        va_start(arg_ptr, firstParameter);
+        for (NSUInteger i = 3; i < signature.numberOfArguments; i++) {
+            void * parameter = va_arg(arg_ptr, void *);
+            [invocation setArgument:parameter atIndex:i];
+        }
+        va_end(arg_ptr);
+    }
+    
+    //6、调用NSinvocation对象
+    [invocation invoke];
+    
+    //7、获取返回值
+    
+    //获得返回值类型
+    const char *returnType = signature.methodReturnType;
+    //声明返回值变量
+    id returnValue;
+    //如果没有返回值，也就是消息声明为void，那么returnValue=nil
+    if(!strcmp(returnType, @encode(void)) ){
+        returnValue =  nil;
+    }
+    //如果返回值为对象，那么为变量赋值
+    else if(!strcmp(returnType, @encode(id)) ){
+        [invocation getReturnValue:&returnValue];
+    }
+    else {
+        //如果返回值为普通类型NSInteger/BOOL等
+        
+        //返回值长度
+        NSUInteger length = [signature methodReturnLength];
+        //根据长度申请内存
+        void *buffer = (void *)malloc(length);
+        //为变量赋值
+        [invocation getReturnValue:buffer];
+        
+        if(!strcmp(returnType, @encode(BOOL)) ) {
+            returnValue = [NSNumber numberWithBool:*((BOOL*)buffer)];
         }
         else if(!strcmp(returnType, @encode(NSInteger)) ){
             returnValue = [NSNumber numberWithInteger:*((NSInteger*)buffer)];
@@ -169,5 +187,297 @@
     }
     return returnValue;
 }
+
+- (BOOL)respondsToSelector:(SEL)aSelector withPre:(NSString *)pre {
+    NSString *selectorString = [NSString stringWithFormat:@"%@%@",pre,NSStringFromSelector(aSelector)];
+    SEL selector = NSSelectorFromString(selectorString);
+    return [self respondsToSelector:selector];
+}
+
+- (id)performSelector:(SEL)aSelector withPre:(NSString *)pre withMethodArgments:(void *)firstParameter, ... {
+    NSString *selectorString = [NSString stringWithFormat:@"%@%@",pre,NSStringFromSelector(aSelector)];
+    SEL selector = NSSelectorFromString(selectorString);
+    //1、创建签名对象
+    NSMethodSignature *signature = [[self class] instanceMethodSignatureForSelector:selector];
+    
+    //2、判断传入的方法是否存在
+    if (signature==nil) {
+        //传入的方法不存在 就抛异常
+        NSString *info = [NSString stringWithFormat:@"-[%@ %@]:unrecognized selector sent to instance",[self class],NSStringFromSelector(selector)];
+        @throw [[NSException alloc] initWithName:@"此方法没有" reason:info userInfo:nil];
+        return nil;
+    }
+    
+    //3、、创建NSInvocation对象
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+    
+    //4、保存方法所属的对象
+    invocation.target = self;
+    invocation.selector = selector;
+    
+    //5、设置参数
+    if (signature.numberOfArguments > 2) {
+        [invocation setArgument:firstParameter atIndex:2];
+        va_list arg_ptr;
+        va_start(arg_ptr, firstParameter);
+        for (NSUInteger i = 3; i < signature.numberOfArguments; i++) {
+            void * parameter = va_arg(arg_ptr, void *);
+            [invocation setArgument:parameter atIndex:i];
+        }
+        va_end(arg_ptr);
+    }
+    
+    //6、调用NSinvocation对象
+    [invocation invoke];
+    
+    //7、获取返回值
+    
+    //获得返回值类型
+    const char *returnType = signature.methodReturnType;
+    //声明返回值变量
+    id returnValue;
+    //如果没有返回值，也就是消息声明为void，那么returnValue=nil
+    if(!strcmp(returnType, @encode(void)) ){
+        returnValue =  nil;
+    }
+    //如果返回值为对象，那么为变量赋值
+    else if(!strcmp(returnType, @encode(id)) ){
+        [invocation getReturnValue:&returnValue];
+    }
+    else {
+        //如果返回值为普通类型NSInteger/BOOL等
+        
+        //返回值长度
+        NSUInteger length = [signature methodReturnLength];
+        //根据长度申请内存
+        void *buffer = (void *)malloc(length);
+        //为变量赋值
+        [invocation getReturnValue:buffer];
+        
+        if(!strcmp(returnType, @encode(BOOL)) ) {
+            returnValue = [NSNumber numberWithBool:*((BOOL*)buffer)];
+        }
+        else if(!strcmp(returnType, @encode(NSInteger)) ){
+            returnValue = [NSNumber numberWithInteger:*((NSInteger*)buffer)];
+        }
+        else if(!strcmp(returnType, @encode(NSUInteger)) ){
+            returnValue = [NSNumber numberWithUnsignedInteger:*((NSUInteger*)buffer)];
+        }
+        else if(!strcmp(returnType, @encode(int))) {
+            returnValue = [NSNumber numberWithInt:*((int*)buffer)];
+        }
+        else if(!strcmp(returnType, @encode(char))) {
+            returnValue = [NSNumber numberWithChar:*((char*)buffer)];
+        }
+        else {
+            returnValue = [NSValue valueWithBytes:buffer objCType:returnType];
+        }
+    }
+    return returnValue;
+}
+
+#pragma --mark 执行类方法
+
++ (id)performClassSelector:(SEL)aSelector withObjects:(NSArray *)objects {
+    
+    //1、创建签名对象
+    NSMethodSignature *signature = [[self class] methodSignatureForSelector:aSelector];
+    
+    //2、判断传入的方法是否存在
+    if (signature==nil) {
+        //传入的方法不存在 就抛异常
+        NSString *info = [NSString stringWithFormat:@"-[%@ %@]:unrecognized selector sent to instance",[self class],NSStringFromSelector(aSelector)];
+        @throw [[NSException alloc] initWithName:@"此方法没有" reason:info userInfo:nil];
+        return nil;
+    }
+    
+    //3、、创建NSInvocation对象
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+    
+    //4、保存方法所属的对象
+    invocation.target = self;
+    invocation.selector = aSelector;
+    
+    //5、设置参数
+    /*
+     当前如果直接遍历参数数组来设置参数
+     如果参数数组元素多余参数个数，那么就会报错
+     */
+    NSInteger arguments =signature.numberOfArguments-2;
+    
+    /*
+     如果直接遍历参数的个数，会存在问题
+     如果参数的个数大于了参数值的个数，那么数组会越界
+     */
+    /*
+     谁少就遍历谁
+     */
+    NSUInteger objectsCount = objects.count;
+    NSInteger count = MIN(arguments, objectsCount);
+    for (int i = 0; i<count; i++) {
+        NSObject*obj = objects[i];
+        //处理参数是NULL类型的情况
+        if ([obj isKindOfClass:[NSNull class]]) {
+            obj = nil;
+        }
+        [invocation setArgument:&obj atIndex:i+2];
+        
+    }
+    
+    //6、调用NSinvocation对象
+    [invocation invoke];
+    
+    //7、获取返回值
+
+    //获得返回值类型
+    const char *returnType = signature.methodReturnType;
+    //声明返回值变量
+    id returnValue;
+    //如果没有返回值，也就是消息声明为void，那么returnValue=nil
+    if(!strcmp(returnType, @encode(void)) ){
+        returnValue =  nil;
+    }
+    //如果返回值为对象，那么为变量赋值
+    else if(!strcmp(returnType, @encode(id)) ){
+        [invocation getReturnValue:&returnValue];
+    }
+    else {
+        //如果返回值为普通类型NSInteger/BOOL等
+        
+        //返回值长度
+        NSUInteger length = [signature methodReturnLength];
+        //根据长度申请内存
+        void *buffer = (void *)malloc(length);
+        //为变量赋值
+        [invocation getReturnValue:buffer];
+        
+        if(!strcmp(returnType, @encode(BOOL)) ) {
+            returnValue = [NSNumber numberWithBool:*((BOOL*)buffer)];
+        }
+        else if(!strcmp(returnType, @encode(NSInteger)) ){
+            returnValue = [NSNumber numberWithInteger:*((NSInteger*)buffer)];
+        }
+        else if(!strcmp(returnType, @encode(NSUInteger)) ){
+            returnValue = [NSNumber numberWithUnsignedInteger:*((NSUInteger*)buffer)];
+        }
+        else if(!strcmp(returnType, @encode(int))) {
+            returnValue = [NSNumber numberWithInt:*((int*)buffer)];
+        }
+        else if(!strcmp(returnType, @encode(char))) {
+            returnValue = [NSNumber numberWithChar:*((char*)buffer)];
+        }
+        else {
+            returnValue = [NSValue valueWithBytes:buffer objCType:returnType];
+        }
+    }
+    return returnValue;
+}
+
+- (id)performClassSelector:(SEL)aSelector withObjects:(NSArray *)objects {
+    return [[self class] performClassSelector:aSelector withObjects:objects];
+}
+
+
+
+
++ (id)performClassSelector:(SEL)aSelector withMethodArgments:(void *)firstParameter, ... {
+    
+    //1、创建签名对象
+    NSMethodSignature *signature = [[self class] methodSignatureForSelector:aSelector];
+    
+    //2、判断传入的方法是否存在
+    if (signature==nil) {
+        //传入的方法不存在 就抛异常
+        NSString *info = [NSString stringWithFormat:@"-[%@ %@]:unrecognized selector sent to instance",[self class],NSStringFromSelector(aSelector)];
+        @throw [[NSException alloc] initWithName:@"此方法没有" reason:info userInfo:nil];
+        return nil;
+    }
+    
+    //3、、创建NSInvocation对象
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+    
+    //4、保存方法所属的对象
+    invocation.target = self;
+    invocation.selector = aSelector;
+    
+    //5、设置参数
+    if (signature.numberOfArguments > 2) {
+        [invocation setArgument:firstParameter atIndex:2];
+        va_list arg_ptr;
+        va_start(arg_ptr, firstParameter);
+        for (NSUInteger i = 3; i < signature.numberOfArguments; i++) {
+            void * parameter = va_arg(arg_ptr, void *);
+            [invocation setArgument:parameter atIndex:i];
+        }
+        va_end(arg_ptr);
+    }
+    
+    //6、调用NSinvocation对象
+    [invocation invoke];
+    
+    //7、获取返回值
+
+    //获得返回值类型
+    const char *returnType = signature.methodReturnType;
+    //声明返回值变量
+    id returnValue;
+    //如果没有返回值，也就是消息声明为void，那么returnValue=nil
+    if(!strcmp(returnType, @encode(void)) ){
+        returnValue =  nil;
+    }
+    //如果返回值为对象，那么为变量赋值
+    else if(!strcmp(returnType, @encode(id)) ){
+        [invocation getReturnValue:&returnValue];
+    }
+    else {
+        //如果返回值为普通类型NSInteger/BOOL等
+        
+        //返回值长度
+        NSUInteger length = [signature methodReturnLength];
+        //根据长度申请内存
+        void *buffer = (void *)malloc(length);
+        //为变量赋值
+        [invocation getReturnValue:buffer];
+        
+        if(!strcmp(returnType, @encode(BOOL)) ) {
+            returnValue = [NSNumber numberWithBool:*((BOOL*)buffer)];
+        }
+        else if(!strcmp(returnType, @encode(NSInteger)) ){
+            returnValue = [NSNumber numberWithInteger:*((NSInteger*)buffer)];
+        }
+        else if(!strcmp(returnType, @encode(NSUInteger)) ){
+            returnValue = [NSNumber numberWithUnsignedInteger:*((NSUInteger*)buffer)];
+        }
+        else if(!strcmp(returnType, @encode(int))) {
+            returnValue = [NSNumber numberWithInt:*((int*)buffer)];
+        }
+        else if(!strcmp(returnType, @encode(char))) {
+            returnValue = [NSNumber numberWithChar:*((char*)buffer)];
+        }
+        else {
+            returnValue = [NSValue valueWithBytes:buffer objCType:returnType];
+        }
+    }
+    return returnValue;
+}
+
+- (id)performClassSelector:(SEL)aSelector withMethodArgments:(void *)firstParameter, ... {
+    return [[self class] performClassSelector:aSelector withMethodArgments:firstParameter];
+}
+
+
+
+
+
++ (id)performClassSelector:(SEL)aSelector withPre:(NSString *)pre withMethodArgments:(void *)firstParameter, ... {
+    NSString *selectorString = [NSString stringWithFormat:@"%@%@",pre,NSStringFromSelector(aSelector)];
+    SEL selector = NSSelectorFromString(selectorString);
+    return [self performSelector:selector withMethodArgments:firstParameter];
+}
+
+- (id)performClassSelector:(SEL)aSelector withPre:(NSString *)pre withMethodArgments:(void *)firstParameter, ... {
+    return [self performSelector:aSelector withPre:pre withMethodArgments:firstParameter];
+}
+
 
 @end
