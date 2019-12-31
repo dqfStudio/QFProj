c68f5c56763ab33a8e8d47952dfacf1b04e22b5d
wind <897432098@qq.com>
2019-12-31 11:07:01 +0700

    modify some files

---
+++ QFProj/Classes/QFUtil/HSkinManager/HSkinManager.h
@@ -0,0 +1,18 @@
+//
+//  HSkinManager.h
+//  QFProj
+//
+//  Created by wind on 2019/12/31.
+//  Copyright © 2019 dqfStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIColor+HUtil.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HSkinManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ QFProj/Classes/QFUtil/HSkinManager/HSkinManager.m
@@ -0,0 +1,126 @@
+//
+//  HSkinManager.m
+//  QFProj
+//
+//  Created by wind on 2019/12/31.
+//  Copyright © 2019 dqfStudio. All rights reserved.
+//
+
+#import "HSkinManager.h"
+
+@implementation HSkinManager
+
+#pragma --mark navigation
++ (UIColor *)naviBarColor {
+    return [UIColor colorWithString:@"#FFFFFF"];
+}
++ (UIColor *)naviBarTitleColor {
+    return [UIColor colorWithString:@"#535353"];
+}
++ (UIColor *)naviBarLeftColor {
+    return [UIColor colorWithString:@"#535353"];
+}
++ (UIColor *)naviBarLeftTitleColor {
+    return [UIColor colorWithString:@"#535353"];
+}
++ (UIColor *)naviBarRightColor {
+    return [UIColor colorWithString:@"#535353"];
+}
++ (UIColor *)naviBarRightTitleColor {
+    return [UIColor colorWithString:@"#535353"];
+}
++ (UIColor *)naviBarLineColor {
+    return [UIColor colorWithString:@"#B7B7B7"];
+}
+
+
+#pragma --mark tabbar
++ (UIColor *)tabBarColor {
+    return [UIColor colorWithString:@"#FFFFFF"];
+}
++ (UIColor *)tabBarTitleNormalColor {
+    return [UIColor colorWithString:@"#FFFFFF"];
+}
++ (UIColor *)tabBarTitleSelectedColor {
+    return [UIColor colorWithString:@"#FFFFFF"];
+}
+
+
+#pragma --mark view controller
++ (UIColor *)vcViewColor {
+    return [UIColor colorWithString:@"#efeff4"];
+}
+
+
+#pragma --mark tuple view
++ (UIColor *)tupleViewColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
++ (UIColor *)tupleSectionColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
++ (UIColor *)tupleHeaderColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
++ (UIColor *)tuplefooterColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
++ (UIColor *)tupleCellColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
+
+
+#pragma --mark label
++ (UIColor *)labelColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
++ (UIColor *)labelTextColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
++ (UIColor *)labelTextDetailColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
++ (UIColor *)labelTextAccessoryColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
+
+
+#pragma --mark button
++ (UIColor *)buttonColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
++ (UIColor *)buttonTextColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
++ (UIColor *)buttonTextDetailColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
++ (UIColor *)buttonTextAccessoryColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
+
+
+#pragma --mark textfield
++ (UIColor *)textFieldColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
++ (UIColor *)textFieldTextColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
++ (UIColor *)textFieldTextDetailColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
++ (UIColor *)textFieldTextAccessoryColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
+
++ (UIColor *)textFieldPlaceholderColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
++ (UIColor *)textFieldPlaceholderDetailColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
++ (UIColor *)textFieldPlaceholderAccessoryColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
+@end
