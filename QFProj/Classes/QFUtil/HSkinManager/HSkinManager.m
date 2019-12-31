4ec2a79bc669cc6a45a1858e260e8c0849f57a44
wind <897432098@qq.com>
2019-12-31 11:35:56 +0700

    modify some files

--- QFProj/Classes/QFUtil/HSkinManager/HSkinManager.m
+++ QFProj/Classes/QFUtil/HSkinManager/HSkinManager.m
@@ -11,116 +11,179 @@
 @implementation HSkinManager
 
 #pragma --mark navigation
+//导航栏背景颜色
 + (UIColor *)naviBarColor {
     return [UIColor colorWithString:@"#FFFFFF"];
 }
+//导航栏标题颜色
 + (UIColor *)naviBarTitleColor {
     return [UIColor colorWithString:@"#535353"];
 }
+//导航栏左边控件背景颜色
 + (UIColor *)naviBarLeftColor {
     return [UIColor colorWithString:@"#535353"];
 }
+//导航栏左边控件标题颜色
 + (UIColor *)naviBarLeftTitleColor {
     return [UIColor colorWithString:@"#535353"];
 }
+//导航朗右边控件背景颜色
 + (UIColor *)naviBarRightColor {
     return [UIColor colorWithString:@"#535353"];
 }
+//导航栏右边控件标题颜色
 + (UIColor *)naviBarRightTitleColor {
     return [UIColor colorWithString:@"#535353"];
 }
-+ (UIColor *)naviBarLineColor {
+//导航栏间隔线颜色
++ (UIColor *)naviBarSeparatorColor {
     return [UIColor colorWithString:@"#B7B7B7"];
 }
 
 
 #pragma --mark tabbar
+//tabBar背景色
 + (UIColor *)tabBarColor {
     return [UIColor colorWithString:@"#FFFFFF"];
 }
+//tabBar标题常规颜色
 + (UIColor *)tabBarTitleNormalColor {
     return [UIColor colorWithString:@"#FFFFFF"];
 }
+//tabBar标题选中颜色
 + (UIColor *)tabBarTitleSelectedColor {
     return [UIColor colorWithString:@"#FFFFFF"];
 }
 
 
 #pragma --mark view controller
+//View controller背景颜色
 + (UIColor *)vcViewColor {
     return [UIColor colorWithString:@"#efeff4"];
 }
 
 
 #pragma --mark tuple view
+//tuple view背景色
 + (UIColor *)tupleViewColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+//tuple view section 背景色
 + (UIColor *)tupleSectionColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+//tuple view header 背景色
 + (UIColor *)tupleHeaderColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+//tuple view footer 背景色
 + (UIColor *)tuplefooterColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+//tuple view cell 背景色
 + (UIColor *)tupleCellColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
+//tuple view cell 间隔线 颜色
++ (UIColor *)tupleCellSeparatorColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
+
+
+#pragma --mark table view
+//table view背景色
++ (UIColor *)tableViewColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
+//table view section 背景色
++ (UIColor *)tableSectionColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
+//table view header 背景色
++ (UIColor *)tableHeaderColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
+//table view footer 背景色
++ (UIColor *)tablefooterColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
+//table view cell 背景色
++ (UIColor *)tableCellColor {
+    return [UIColor colorWithString:@"#17171B"];
+}
+//table view 间隔线 颜色
++ (UIColor *)tableCellSeparatorColor {
     return [UIColor colorWithString:@"#17171B"];
 }
 
 
 #pragma --mark label
+//label背景色
 + (UIColor *)labelColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+//label text 颜色
 + (UIColor *)labelTextColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+//label text detail 颜色
 + (UIColor *)labelTextDetailColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+//label text accessory 颜色
 + (UIColor *)labelTextAccessoryColor {
     return [UIColor colorWithString:@"#17171B"];
 }
 
 
 #pragma --mark button
+//button背景色
 + (UIColor *)buttonColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+//button text 颜色
 + (UIColor *)buttonTextColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+//button text detail 颜色
 + (UIColor *)buttonTextDetailColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+//button text accessory 颜色
 + (UIColor *)buttonTextAccessoryColor {
     return [UIColor colorWithString:@"#17171B"];
 }
 
 
 #pragma --mark textfield
+//textField背景色
 + (UIColor *)textFieldColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+//textField text 颜色
 + (UIColor *)textFieldTextColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+//textField detail 颜色
 + (UIColor *)textFieldTextDetailColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+//textField accessory 颜色
 + (UIColor *)textFieldTextAccessoryColor {
     return [UIColor colorWithString:@"#17171B"];
 }
 
+//textFieldPlaceholder 颜色
 + (UIColor *)textFieldPlaceholderColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+//textFieldPlaceholder detail 颜色
 + (UIColor *)textFieldPlaceholderDetailColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+//textFieldPlaceholder accessory 颜色
 + (UIColor *)textFieldPlaceholderAccessoryColor {
     return [UIColor colorWithString:@"#17171B"];
 }
+
 @end
