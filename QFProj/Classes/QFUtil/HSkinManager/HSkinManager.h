75c3002b31c98a126648ba9be7d99a80f4393dc5
wind <897432098@qq.com>
2020-01-13 14:09:49 +0700

    modify some files

--- QFProj/Classes/QFUtil/HSkinManager/HSkinManager.h
+++ QFProj/Classes/QFUtil/HSkinManager/HSkinManager.h
@@ -11,104 +11,261 @@
 
 @interface HSkinManager : NSObject
 
-#pragma --mark navigation
+#pragma --mark naviBar
 //导航栏背景颜色
 + (UIColor *)naviBarColor;
++ (UIColor *)naviBarDetailColor;
++ (UIColor *)naviBarAccessoryColor;
++ (UIColor *)naviBarExtra1Color;
++ (UIColor *)naviBarExtra2Color;
 //导航栏标题颜色
 + (UIColor *)naviBarTitleColor;
++ (UIColor *)naviBarTitleDetailColor;
++ (UIColor *)naviBarTitleAccessoryColor;
++ (UIColor *)naviBarTitleExtra1Color;
++ (UIColor *)naviBarTitleExtra2Color;
 //导航栏左边控件背景颜色
 + (UIColor *)naviBarLeftColor;
++ (UIColor *)naviBarLeftDetailColor;
++ (UIColor *)naviBarLeftAccessoryColor;
++ (UIColor *)naviBarLeftExtra1Color;
++ (UIColor *)naviBarLeftExtra2Color;
 //导航栏左边控件标题颜色
 + (UIColor *)naviBarLeftTitleColor;
++ (UIColor *)naviBarLeftTitleDetailColor;
++ (UIColor *)naviBarLeftTitleAccessoryColor;
++ (UIColor *)naviBarLeftTitleExtra1Color;
++ (UIColor *)naviBarLeftTitleExtra2Color;
 //导航朗右边控件背景颜色
 + (UIColor *)naviBarRightColor;
++ (UIColor *)naviBarRightDetailColor;
++ (UIColor *)naviBarRightAccessoryColor;
++ (UIColor *)naviBarRightExtra1Color;
++ (UIColor *)naviBarRightExtra2Color;
 //导航栏右边控件标题颜色
 + (UIColor *)naviBarRightTitleColor;
++ (UIColor *)naviBarRightTitleDetailColor;
++ (UIColor *)naviBarRightTitleAccessoryColor;
++ (UIColor *)naviBarRightTitleExtra1Color;
++ (UIColor *)naviBarRightTitleExtra2Color;
 //导航栏间隔线颜色
 + (UIColor *)naviBarSeparatorColor;
-
++ (UIColor *)naviBarSeparatorDetailColor;
++ (UIColor *)naviBarSeparatorAccessoryColor;
++ (UIColor *)naviBarSeparatorExtra1Color;
++ (UIColor *)naviBarSeparatorExtra2Color;
 
 #pragma --mark tabbar
 //tabBar背景色
 + (UIColor *)tabBarColor;
++ (UIColor *)tabBarDetailColor;
++ (UIColor *)tabBarAccessoryColor;
++ (UIColor *)tabBarExtra1Color;
++ (UIColor *)tabBarExtra2Color;
 //tabBar标题常规颜色
 + (UIColor *)tabBarTitleNormalColor;
++ (UIColor *)tabBarTitleNormalDetailColor;
++ (UIColor *)tabBarTitleNormalAccessoryColor;
++ (UIColor *)tabBarTitleNormalExtra1Color;
++ (UIColor *)tabBarTitleNormalExtra2Color;
 //tabBar标题选中颜色
 + (UIColor *)tabBarTitleSelectedColor;
-
++ (UIColor *)tabBarTitleSelectedDetailColor;
++ (UIColor *)tabBarTitleSelectedAccessoryColor;
++ (UIColor *)tabBarTitleSelectedExtra1Color;
++ (UIColor *)tabBarTitleSelectedExtra2Color;
 
 #pragma --mark view controller
 //View controller背景颜色
 + (UIColor *)vcViewColor;
++ (UIColor *)vcViewDetailColor;
++ (UIColor *)vcViewAccessoryColor;
++ (UIColor *)vcViewExtra1Color;
++ (UIColor *)vcViewExtra2Color;
 
+#pragma --mark view color
+// view border color 颜色
++ (UIColor *)viewBorderColor;
++ (UIColor *)viewBorderDetailColor;
++ (UIColor *)viewBorderAccessoryColor;
++ (UIColor *)viewBorderExtra1Color;
++ (UIColor *)viewBorderExtra2Color;
+
+// view stroke color 颜色
++ (UIColor *)viewStrokeColor;
++ (UIColor *)viewStrokeDetailColor;
++ (UIColor *)viewStrokeAccessoryColor;
++ (UIColor *)viewStrokeExtra1Color;
++ (UIColor *)viewStrokeExtra2Color;
+
+// view line color 颜色
++ (UIColor *)viewLineColor;
++ (UIColor *)viewLineDetailColor;
++ (UIColor *)viewLineAccessoryColor;
++ (UIColor *)viewLineExtra1Color;
++ (UIColor *)viewLineExtra2Color;
 
 #pragma --mark tuple view
 //tuple view背景色
-+ (UIColor *)tupleViewColor;
++ (UIColor *)tupleColor;
++ (UIColor *)tupleDetailColor;
++ (UIColor *)tupleAccessoryColor;
++ (UIColor *)tupleExtra1Color;
++ (UIColor *)tupleExtra2Color;
 //tuple view section 背景色
 + (UIColor *)tupleSectionColor;
++ (UIColor *)tupleSectionDetailColor;
++ (UIColor *)tupleSectionAccessoryColor;
++ (UIColor *)tupleSectionExtra1Color;
++ (UIColor *)tupleSectionExtra2Color;
 //tuple view header 背景色
 + (UIColor *)tupleHeaderColor;
++ (UIColor *)tupleHeaderDetailColor;
++ (UIColor *)tupleHeaderAccessoryColor;
++ (UIColor *)tupleHeaderExtra1Color;
++ (UIColor *)tupleHeaderExtra2Color;
 //tuple view footer 背景色
-+ (UIColor *)tuplefooterColor;
++ (UIColor *)tupleFooterColor;
++ (UIColor *)tupleFooterDetailColor;
++ (UIColor *)tupleFooterAccessoryColor;
++ (UIColor *)tupleFooterExtra1Color;
++ (UIColor *)tupleFooterExtra2Color;
 //tuple view cell 背景色
 + (UIColor *)tupleCellColor;
++ (UIColor *)tupleCellDetailColor;
++ (UIColor *)tupleCellAccessoryColor;
++ (UIColor *)tupleCellExtra1Color;
++ (UIColor *)tupleCellExtra2Color;
 //tuple view cell 间隔线 颜色
 + (UIColor *)tupleCellSeparatorColor;
-
++ (UIColor *)tupleCellSeparatorDetailColor;
++ (UIColor *)tupleCellSeparatorAccessoryColor;
++ (UIColor *)tupleCellSeparatorExtra1Color;
++ (UIColor *)tupleCellSeparatorExtra2Color;
 
 #pragma --mark table view
 //table view背景色
-+ (UIColor *)tableViewColor;
++ (UIColor *)tableColor;
++ (UIColor *)tableDetailColor;
++ (UIColor *)tableAccessoryColor;
++ (UIColor *)tableExtra1Color;
++ (UIColor *)tableExtra2Color;
 //table view section 背景色
 + (UIColor *)tableSectionColor;
++ (UIColor *)tableSectionDetailColor;
++ (UIColor *)tableSectionAccessoryColor;
++ (UIColor *)tableSectionExtra1Color;
++ (UIColor *)tableSectionExtra2Color;
 //table view header 背景色
 + (UIColor *)tableHeaderColor;
++ (UIColor *)tableHeaderDetailColor;
++ (UIColor *)tableHeaderAccessoryColor;
++ (UIColor *)tableHeaderExtra1Color;
++ (UIColor *)tableHeaderExtra2Color;
 //table view footer 背景色
-+ (UIColor *)tablefooterColor;
++ (UIColor *)tableFooterColor;
++ (UIColor *)tableFooterDetailColor;
++ (UIColor *)tableFooterAccessoryColor;
++ (UIColor *)tableFooterExtra1Color;
++ (UIColor *)tableFooterExtra2Color;
 //table view cell 背景色
 + (UIColor *)tableCellColor;
-//table view 间隔线 颜色
++ (UIColor *)tableCellDetailColor;
++ (UIColor *)tableCellAccessoryColor;
++ (UIColor *)tableCellExtra1Color;
++ (UIColor *)tableCellExtra2Color;
+//table view cell 间隔线 颜色
 + (UIColor *)tableCellSeparatorColor;
-
++ (UIColor *)tableCellSeparatorDetailColor;
++ (UIColor *)tableCellSeparatorAccessoryColor;
++ (UIColor *)tableCellSeparatorExtra1Color;
++ (UIColor *)tableCellSeparatorExtra2Color;
 
 #pragma --mark label
 //label背景色
 + (UIColor *)labelColor;
-//label text 颜色
-+ (UIColor *)labelTextColor;
-//label text detail 颜色
-+ (UIColor *)labelTextDetailColor;
-//label text accessory 颜色
-+ (UIColor *)labelTextAccessoryColor;
++ (UIColor *)labelDetailColor;
++ (UIColor *)labelAccessoryColor;
++ (UIColor *)labelExtra1Color;
++ (UIColor *)labelExtra2Color;
 
+//label text normal 颜色
++ (UIColor *)labelNormalColor;
++ (UIColor *)labelNormalDetailColor;
++ (UIColor *)labelNormalAccessoryColor;
++ (UIColor *)labelNormalExtra1Color;
++ (UIColor *)labelNormalExtra2Color;
+
+//label text selected 颜色
++ (UIColor *)labelSelectedColor;
++ (UIColor *)labelSelectedDetailColor;
++ (UIColor *)labelSelectedAccessoryColor;
++ (UIColor *)labelSelectedExtra1Color;
++ (UIColor *)labelSelectedExtra2Color;
+
+//label text highlighted 颜色
++ (UIColor *)labelHighlightedColor;
++ (UIColor *)labelHighlightedDetailColor;
++ (UIColor *)labelHighlightedAccessoryColor;
++ (UIColor *)labelHighlightedExtra1Color;
++ (UIColor *)labelHighlightedExtra2Color;
 
 #pragma --mark button
 //button背景色
 + (UIColor *)buttonColor;
-//button text 颜色
-+ (UIColor *)buttonTextColor;
-//button text detail 颜色
-+ (UIColor *)buttonTextDetailColor;
-//button text accessory 颜色
-+ (UIColor *)buttonTextAccessoryColor;
++ (UIColor *)buttonDetailColor;
++ (UIColor *)buttonAccessoryColor;
++ (UIColor *)buttonExtra1Color;
++ (UIColor *)buttonExtra2Color;
 
+// button render 颜色
++ (UIColor *)buttonRenderColor;
++ (UIColor *)buttonRenderDetailColor;
++ (UIColor *)buttonRenderAccessoryColor;
++ (UIColor *)buttonRenderExtra1Color;
++ (UIColor *)buttonRenderExtra2Color;
+
+//button text normal 颜色
++ (UIColor *)buttonNormalColor;
++ (UIColor *)buttonNormalDetailColor;
++ (UIColor *)buttonNormalAccessoryColor;
++ (UIColor *)buttonNormalExtra1Color;
++ (UIColor *)buttonNormalExtra2Color;
+
+//button text selected 颜色
++ (UIColor *)buttonSelectedColor;
++ (UIColor *)buttonSelectedDetailColor;
++ (UIColor *)buttonSelectedAccessoryColor;
++ (UIColor *)buttonSelectedExtra1Color;
++ (UIColor *)buttonSelectedExtra2Color;
+
+//button text highlighted 颜色
++ (UIColor *)buttonHighlightedColor;
++ (UIColor *)buttonHighlightedDetailColor;
++ (UIColor *)buttonHighlightedAccessoryColor;
++ (UIColor *)buttonHighlightedExtra1Color;
++ (UIColor *)buttonHighlightedExtra2Color;
 
 #pragma --mark textfield
 //textField背景色
 + (UIColor *)textFieldColor;
++ (UIColor *)textFieldDetailColor;
++ (UIColor *)textFieldAccessoryColor;
++ (UIColor *)textFieldExtra1Color;
++ (UIColor *)textFieldExtra2Color;
+
 //textField text 颜色
 + (UIColor *)textFieldTextColor;
-//textField detail 颜色
 + (UIColor *)textFieldTextDetailColor;
-//textField accessory 颜色
 + (UIColor *)textFieldTextAccessoryColor;
++ (UIColor *)textFieldTextExtra1Color;
++ (UIColor *)textFieldTextExtra2Color;
 
 //textFieldPlaceholder 颜色
 + (UIColor *)textFieldPlaceholderColor;
-//textFieldPlaceholder detail 颜色
 + (UIColor *)textFieldPlaceholderDetailColor;
-//textFieldPlaceholder accessory 颜色
 + (UIColor *)textFieldPlaceholderAccessoryColor;
++ (UIColor *)textFieldPlaceholderExtra1Color;
++ (UIColor *)textFieldPlaceholderExtra2Color;
 
 @end
--- QFProj/Classes/QFUtil/HSkinManager/HSkinManager.m
+++ QFProj/Classes/QFUtil/HSkinManager/HSkinManager.m
@@ -10,180 +10,641 @@
 
 @implementation HSkinManager
 
-#pragma --mark navigation
+#pragma --mark naviBar
 //导航栏背景颜色
 + (UIColor *)naviBarColor {
-    return [UIColor colorWithString:@"#FFFFFF"];
+    return nil;
+}
++ (UIColor *)naviBarDetailColor {
+    return nil;
+}
++ (UIColor *)naviBarAccessoryColor {
+    return nil;
+}
++ (UIColor *)naviBarExtra1Color {
+    return nil;
+}
++ (UIColor *)naviBarExtra2Color {
+    return nil;
 }
 //导航栏标题颜色
 + (UIColor *)naviBarTitleColor {
-    return [UIColor colorWithString:@"#535353"];
+    return nil;
+}
++ (UIColor *)naviBarTitleDetailColor {
+    return nil;
+}
++ (UIColor *)naviBarTitleAccessoryColor {
+    return nil;
+}
++ (UIColor *)naviBarTitleExtra1Color {
+    return nil;
+}
++ (UIColor *)naviBarTitleExtra2Color {
+    return nil;
 }
 //导航栏左边控件背景颜色
 + (UIColor *)naviBarLeftColor {
-    return [UIColor colorWithString:@"#535353"];
+    return nil;
+}
++ (UIColor *)naviBarLeftDetailColor {
+    return nil;
+}
++ (UIColor *)naviBarLeftAccessoryColor {
+    return nil;
+}
++ (UIColor *)naviBarLeftExtra1Color {
+    return nil;
+}
++ (UIColor *)naviBarLeftExtra2Color {
+    return nil;
 }
 //导航栏左边控件标题颜色
 + (UIColor *)naviBarLeftTitleColor {
-    return [UIColor colorWithString:@"#535353"];
+    return nil;
+}
++ (UIColor *)naviBarLeftTitleDetailColor {
+    return nil;
+}
++ (UIColor *)naviBarLeftTitleAccessoryColor {
+    return nil;
+}
++ (UIColor *)naviBarLeftTitleExtra1Color {
+    return nil;
+}
++ (UIColor *)naviBarLeftTitleExtra2Color {
+    return nil;
 }
 //导航朗右边控件背景颜色
 + (UIColor *)naviBarRightColor {
-    return [UIColor colorWithString:@"#535353"];
+    return nil;
+}
++ (UIColor *)naviBarRightDetailColor {
+    return nil;
+}
++ (UIColor *)naviBarRightAccessoryColor {
+    return nil;
+}
++ (UIColor *)naviBarRightExtra1Color {
+    return nil;
+}
++ (UIColor *)naviBarRightExtra2Color {
+    return nil;
 }
 //导航栏右边控件标题颜色
 + (UIColor *)naviBarRightTitleColor {
-    return [UIColor colorWithString:@"#535353"];
+    return nil;
+}
++ (UIColor *)naviBarRightTitleDetailColor {
+    return nil;
+}
++ (UIColor *)naviBarRightTitleAccessoryColor {
+    return nil;
+}
++ (UIColor *)naviBarRightTitleExtra1Color {
+    return nil;
+}
++ (UIColor *)naviBarRightTitleExtra2Color {
+    return nil;
 }
 //导航栏间隔线颜色
 + (UIColor *)naviBarSeparatorColor {
-    return [UIColor colorWithString:@"#B7B7B7"];
+    return nil;
 }
-
++ (UIColor *)naviBarSeparatorDetailColor {
+    return nil;
+}
++ (UIColor *)naviBarSeparatorAccessoryColor {
+    return nil;
+}
++ (UIColor *)naviBarSeparatorExtra1Color {
+    return nil;
+}
++ (UIColor *)naviBarSeparatorExtra2Color {
+    return nil;
+}
 
 #pragma --mark tabbar
 //tabBar背景色
 + (UIColor *)tabBarColor {
-    return [UIColor colorWithString:@"#FFFFFF"];
+    return nil;
+}
++ (UIColor *)tabBarDetailColor {
+    return nil;
+}
++ (UIColor *)tabBarAccessoryColor {
+    return nil;
+}
++ (UIColor *)tabBarExtra1Color {
+    return nil;
+}
++ (UIColor *)tabBarExtra2Color {
+    return nil;
 }
 //tabBar标题常规颜色
 + (UIColor *)tabBarTitleNormalColor {
-    return [UIColor colorWithString:@"#FFFFFF"];
+    return nil;
+}
++ (UIColor *)tabBarTitleNormalDetailColor {
+    return nil;
+}
++ (UIColor *)tabBarTitleNormalAccessoryColor {
+    return nil;
+}
++ (UIColor *)tabBarTitleNormalExtra1Color {
+    return nil;
+}
++ (UIColor *)tabBarTitleNormalExtra2Color {
+    return nil;
 }
 //tabBar标题选中颜色
 + (UIColor *)tabBarTitleSelectedColor {
-    return [UIColor colorWithString:@"#FFFFFF"];
+    return nil;
 }
-
++ (UIColor *)tabBarTitleSelectedDetailColor {
+    return nil;
+}
++ (UIColor *)tabBarTitleSelectedAccessoryColor {
+    return nil;
+}
++ (UIColor *)tabBarTitleSelectedExtra1Color {
+    return nil;
+}
++ (UIColor *)tabBarTitleSelectedExtra2Color {
+    return nil;
+}
 
 #pragma --mark view controller
 //View controller背景颜色
 + (UIColor *)vcViewColor {
-    return [UIColor colorWithString:@"#efeff4"];
+    return nil;
+}
++ (UIColor *)vcViewDetailColor {
+    return nil;
+}
++ (UIColor *)vcViewAccessoryColor {
+    return nil;
+}
++ (UIColor *)vcViewExtra1Color {
+    return nil;
+}
++ (UIColor *)vcViewExtra2Color {
+    return nil;
 }
 
+#pragma --mark view color
+// view border color 颜色
++ (UIColor *)viewBorderColor {
+    return nil;
+}
++ (UIColor *)viewBorderDetailColor {
+    return nil;
+}
++ (UIColor *)viewBorderAccessoryColor {
+    return nil;
+}
++ (UIColor *)viewBorderExtra1Color {
+    return nil;
+}
++ (UIColor *)viewBorderExtra2Color {
+    return nil;
+}
+
+// view stroke color 颜色
++ (UIColor *)viewStrokeColor {
+    return nil;
+}
++ (UIColor *)viewStrokeDetailColor {
+    return nil;
+}
++ (UIColor *)viewStrokeAccessoryColor {
+    return nil;
+}
++ (UIColor *)viewStrokeExtra1Color {
+    return nil;
+}
++ (UIColor *)viewStrokeExtra2Color {
+    return nil;
+}
+
+// view line color 颜色
++ (UIColor *)viewLineColor {
+    return nil;
+}
++ (UIColor *)viewLineDetailColor {
+    return nil;
+}
++ (UIColor *)viewLineAccessoryColor {
+    return nil;
+}
++ (UIColor *)viewLineExtra1Color {
+    return nil;
+}
++ (UIColor *)viewLineExtra2Color {
+    return nil;
+}
 
 #pragma --mark tuple view
 //tuple view背景色
-+ (UIColor *)tupleViewColor {
-    return [UIColor colorWithString:@"#17171B"];
++ (UIColor *)tupleColor {
+    return nil;
+}
++ (UIColor *)tupleDetailColor {
+    return nil;
+}
++ (UIColor *)tupleAccessoryColor {
+    return nil;
+}
++ (UIColor *)tupleExtra1Color {
+    return nil;
+}
++ (UIColor *)tupleExtra2Color {
+    return nil;
 }
 //tuple view section 背景色
 + (UIColor *)tupleSectionColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
+}
++ (UIColor *)tupleSectionDetailColor {
+    return nil;
+}
++ (UIColor *)tupleSectionAccessoryColor {
+    return nil;
+}
++ (UIColor *)tupleSectionExtra1Color {
+    return nil;
+}
++ (UIColor *)tupleSectionExtra2Color {
+    return nil;
 }
 //tuple view header 背景色
 + (UIColor *)tupleHeaderColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
+}
++ (UIColor *)tupleHeaderDetailColor {
+    return nil;
+}
++ (UIColor *)tupleHeaderAccessoryColor {
+    return nil;
+}
++ (UIColor *)tupleHeaderExtra1Color {
+    return nil;
+}
++ (UIColor *)tupleHeaderExtra2Color {
+    return nil;
 }
 //tuple view footer 背景色
-+ (UIColor *)tuplefooterColor {
-    return [UIColor colorWithString:@"#17171B"];
++ (UIColor *)tupleFooterColor {
+    return nil;
+}
++ (UIColor *)tupleFooterDetailColor {
+    return nil;
+}
++ (UIColor *)tupleFooterAccessoryColor {
+    return nil;
+}
++ (UIColor *)tupleFooterExtra1Color {
+    return nil;
+}
++ (UIColor *)tupleFooterExtra2Color {
+    return nil;
 }
 //tuple view cell 背景色
 + (UIColor *)tupleCellColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
+}
++ (UIColor *)tupleCellDetailColor {
+    return nil;
+}
++ (UIColor *)tupleCellAccessoryColor {
+    return nil;
+}
++ (UIColor *)tupleCellExtra1Color {
+    return nil;
+}
++ (UIColor *)tupleCellExtra2Color {
+    return nil;
 }
 //tuple view cell 间隔线 颜色
 + (UIColor *)tupleCellSeparatorColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
 }
-
++ (UIColor *)tupleCellSeparatorDetailColor {
+    return nil;
+}
++ (UIColor *)tupleCellSeparatorAccessoryColor {
+    return nil;
+}
++ (UIColor *)tupleCellSeparatorExtra1Color {
+    return nil;
+}
++ (UIColor *)tupleCellSeparatorExtra2Color {
+    return nil;
+}
 
 #pragma --mark table view
 //table view背景色
-+ (UIColor *)tableViewColor {
-    return [UIColor colorWithString:@"#17171B"];
++ (UIColor *)tableColor {
+    return nil;
+}
++ (UIColor *)tableDetailColor {
+    return nil;
+}
++ (UIColor *)tableAccessoryColor {
+    return nil;
+}
++ (UIColor *)tableExtra1Color {
+    return nil;
+}
++ (UIColor *)tableExtra2Color {
+    return nil;
 }
 //table view section 背景色
 + (UIColor *)tableSectionColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
+}
++ (UIColor *)tableSectionDetailColor {
+    return nil;
+}
++ (UIColor *)tableSectionAccessoryColor {
+    return nil;
+}
++ (UIColor *)tableSectionExtra1Color {
+    return nil;
+}
++ (UIColor *)tableSectionExtra2Color {
+    return nil;
 }
 //table view header 背景色
 + (UIColor *)tableHeaderColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
+}
++ (UIColor *)tableHeaderDetailColor {
+    return nil;
+}
++ (UIColor *)tableHeaderAccessoryColor {
+    return nil;
+}
++ (UIColor *)tableHeaderExtra1Color {
+    return nil;
+}
++ (UIColor *)tableHeaderExtra2Color {
+    return nil;
 }
 //table view footer 背景色
-+ (UIColor *)tablefooterColor {
-    return [UIColor colorWithString:@"#17171B"];
++ (UIColor *)tableFooterColor {
+    return nil;
+}
++ (UIColor *)tableFooterDetailColor {
+    return nil;
+}
++ (UIColor *)tableFooterAccessoryColor {
+    return nil;
+}
++ (UIColor *)tableFooterExtra1Color {
+    return nil;
+}
++ (UIColor *)tableFooterExtra2Color {
+    return nil;
 }
 //table view cell 背景色
 + (UIColor *)tableCellColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
 }
-//table view 间隔线 颜色
++ (UIColor *)tableCellDetailColor {
+    return nil;
+}
++ (UIColor *)tableCellAccessoryColor {
+    return nil;
+}
++ (UIColor *)tableCellExtra1Color {
+    return nil;
+}
++ (UIColor *)tableCellExtra2Color {
+    return nil;
+}
+//table view cell 间隔线 颜色
 + (UIColor *)tableCellSeparatorColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
 }
-
++ (UIColor *)tableCellSeparatorDetailColor {
+    return nil;
+}
++ (UIColor *)tableCellSeparatorAccessoryColor {
+    return nil;
+}
++ (UIColor *)tableCellSeparatorExtra1Color {
+    return nil;
+}
++ (UIColor *)tableCellSeparatorExtra2Color {
+    return nil;
+}
 
 #pragma --mark label
 //label背景色
 + (UIColor *)labelColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
 }
-//label text 颜色
-+ (UIColor *)labelTextColor {
-    return [UIColor colorWithString:@"#17171B"];
++ (UIColor *)labelDetailColor {
+    return nil;
 }
-//label text detail 颜色
-+ (UIColor *)labelTextDetailColor {
-    return [UIColor colorWithString:@"#17171B"];
++ (UIColor *)labelAccessoryColor {
+    return nil;
 }
-//label text accessory 颜色
-+ (UIColor *)labelTextAccessoryColor {
-    return [UIColor colorWithString:@"#17171B"];
++ (UIColor *)labelExtra1Color {
+    return nil;
+}
++ (UIColor *)labelExtra2Color {
+    return nil;
 }
 
+//label text normal 颜色
++ (UIColor *)labelNormalColor {
+    return nil;
+}
++ (UIColor *)labelNormalDetailColor {
+    return nil;
+}
++ (UIColor *)labelNormalAccessoryColor {
+    return nil;
+}
++ (UIColor *)labelNormalExtra1Color {
+    return nil;
+}
++ (UIColor *)labelNormalExtra2Color {
+    return nil;
+}
+
+//label text selected 颜色
++ (UIColor *)labelSelectedColor {
+    return nil;
+}
++ (UIColor *)labelSelectedDetailColor {
+    return nil;
+}
++ (UIColor *)labelSelectedAccessoryColor {
+    return nil;
+}
++ (UIColor *)labelSelectedExtra1Color {
+    return nil;
+}
++ (UIColor *)labelSelectedExtra2Color {
+    return nil;
+}
+
+//label text highlighted 颜色
++ (UIColor *)labelHighlightedColor {
+    return nil;
+}
++ (UIColor *)labelHighlightedDetailColor {
+    return nil;
+}
++ (UIColor *)labelHighlightedAccessoryColor {
+    return nil;
+}
++ (UIColor *)labelHighlightedExtra1Color {
+    return nil;
+}
++ (UIColor *)labelHighlightedExtra2Color {
+    return nil;
+}
 
 #pragma --mark button
 //button背景色
 + (UIColor *)buttonColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
 }
-//button text 颜色
-+ (UIColor *)buttonTextColor {
-    return [UIColor colorWithString:@"#17171B"];
++ (UIColor *)buttonDetailColor {
+    return nil;
 }
-//button text detail 颜色
-+ (UIColor *)buttonTextDetailColor {
-    return [UIColor colorWithString:@"#17171B"];
++ (UIColor *)buttonAccessoryColor {
+    return nil;
 }
-//button text accessory 颜色
-+ (UIColor *)buttonTextAccessoryColor {
-    return [UIColor colorWithString:@"#17171B"];
++ (UIColor *)buttonExtra1Color {
+    return nil;
+}
++ (UIColor *)buttonExtra2Color {
+    return nil;
 }
 
+// button render 颜色
++ (UIColor *)buttonRenderColor {
+    return nil;
+}
++ (UIColor *)buttonRenderDetailColor {
+    return nil;
+}
++ (UIColor *)buttonRenderAccessoryColor {
+    return nil;
+}
++ (UIColor *)buttonRenderExtra1Color {
+    return nil;
+}
++ (UIColor *)buttonRenderExtra2Color {
+    return nil;
+}
+
+//button text normal 颜色
++ (UIColor *)buttonNormalColor {
+    return nil;
+}
++ (UIColor *)buttonNormalDetailColor {
+    return nil;
+}
++ (UIColor *)buttonNormalAccessoryColor {
+    return nil;
+}
++ (UIColor *)buttonNormalExtra1Color {
+    return nil;
+}
++ (UIColor *)buttonNormalExtra2Color {
+    return nil;
+}
+
+//button text selected 颜色
++ (UIColor *)buttonSelectedColor {
+    return nil;
+}
++ (UIColor *)buttonSelectedDetailColor {
+    return nil;
+}
++ (UIColor *)buttonSelectedAccessoryColor {
+    return nil;
+}
++ (UIColor *)buttonSelectedExtra1Color {
+    return nil;
+}
++ (UIColor *)buttonSelectedExtra2Color {
+    return nil;
+}
+
+//button text highlighted 颜色
++ (UIColor *)buttonHighlightedColor {
+    return nil;
+}
++ (UIColor *)buttonHighlightedDetailColor {
+    return nil;
+}
++ (UIColor *)buttonHighlightedAccessoryColor {
+    return nil;
+}
++ (UIColor *)buttonHighlightedExtra1Color {
+    return nil;
+}
++ (UIColor *)buttonHighlightedExtra2Color {
+    return nil;
+}
 
 #pragma --mark textfield
 //textField背景色
 + (UIColor *)textFieldColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
 }
++ (UIColor *)textFieldDetailColor {
+    return nil;
+}
++ (UIColor *)textFieldAccessoryColor {
+    return nil;
+}
++ (UIColor *)textFieldExtra1Color {
+    return nil;
+}
++ (UIColor *)textFieldExtra2Color {
+    return nil;
+}
+
 //textField text 颜色
 + (UIColor *)textFieldTextColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
 }
-//textField detail 颜色
 + (UIColor *)textFieldTextDetailColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
 }
-//textField accessory 颜色
 + (UIColor *)textFieldTextAccessoryColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
+}
++ (UIColor *)textFieldTextExtra1Color {
+    return nil;
+}
++ (UIColor *)textFieldTextExtra2Color {
+    return nil;
 }
 
 //textFieldPlaceholder 颜色
 + (UIColor *)textFieldPlaceholderColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
 }
-//textFieldPlaceholder detail 颜色
 + (UIColor *)textFieldPlaceholderDetailColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
 }
-//textFieldPlaceholder accessory 颜色
 + (UIColor *)textFieldPlaceholderAccessoryColor {
-    return [UIColor colorWithString:@"#17171B"];
+    return nil;
+}
++ (UIColor *)textFieldPlaceholderExtra1Color {
+    return nil;
+}
++ (UIColor *)textFieldPlaceholderExtra2Color {
+    return nil;
 }
 
 @end
