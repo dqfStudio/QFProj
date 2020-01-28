3346f78a7136de5ba2c94b675fc2da0cb88797f7
wind <897432098@qq.com>
2020-01-28 17:45:15 +0700

    modify some files

---
+++ QFProj/Classes/QFUtil/HJsonManager/HJsonFileManager.h
@@ -0,0 +1,19 @@
+//
+//  HJsonFileManager.h
+//  QFProj
+//
+//  Created by wind on 2019/12/13.
+//  Copyright © 2019 dqfStudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HJsonFileManager : NSObject
++ (instancetype)share;
+- (nullable id)resourceWithName:(NSString *)name; //加载资源
+- (void)releaseResource:(NSString *)name; //释放资源
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ QFProj/Classes/QFUtil/HJsonManager/HJsonFileManager.m
@@ -0,0 +1,53 @@
+//
+//  HJsonFileManager.m
+//  QFProj
+//
+//  Created by wind on 2019/12/13.
+//  Copyright © 2019 dqfStudio. All rights reserved.
+//
+
+#import "HJsonFileManager.h"
+
+@interface HJsonFileManager ()
+@property (nonatomic) NSMutableDictionary *mutableDict;
+@end
+
+@implementation HJsonFileManager
++ (instancetype)share {
+    static id sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init];
+    });
+    return sharedInstance;
+}
+- (NSMutableDictionary *)mutableDict {
+    if (!_mutableDict) {
+        _mutableDict = NSMutableDictionary.dictionary;
+    }
+    return _mutableDict;
+}
+- (nullable id)resourceWithName:(NSString *)name {
+    if (![name isKindOfClass:NSString.class]) return nil;
+    id resource = [self.mutableDict objectForKey:name];
+    if (!resource) {
+        NSString *path = [[NSBundle mainBundle] pathForResource:name ofType:nil];
+        if (path) {
+            NSData *data = [NSData dataWithContentsOfFile:path];
+            if (data) {
+                resource = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+                if ([resource isKindOfClass:NSArray.class] || [resource isKindOfClass:NSDictionary.class]) {
+                    [self.mutableDict setObject:resource forKey:name];
+                }
+            }
+        }
+    }
+    return resource;
+}
+- (void)releaseResource:(NSString *)name {
+    if (![name isKindOfClass:NSString.class]) return;
+    if ([self.mutableDict.allKeys containsObject:name]) {
+        [self.mutableDict removeObjectForKey:name];
+    }
+}
+@end
