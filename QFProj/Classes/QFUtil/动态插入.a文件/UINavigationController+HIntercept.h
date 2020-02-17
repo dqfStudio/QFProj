d56f819c77cfa35cc855e9de448fb02414eddba9
wind <897432098@qq.com>
2020-02-17 15:56:14 +0700

    modify some files

---
+++ QFProj/Classes/QFUtil/动态插入.a文件/UINavigationController+HIntercept.h
@@ -0,0 +1,7 @@
+//
+//#import <UIKit/UIKit.h>
+//
+////该类为要拦截处理的类
+//@interface HUIViewController : UIViewController
+//
+//@end
---
+++ QFProj/Classes/QFUtil/动态插入.a文件/UINavigationController+HIntercept.m
@@ -0,0 +1,107 @@
+//
+//#import "UINavigationController+HIntercept.h"
+//#import <objc/runtime.h>
+//
+//void HInterceptSwizzleInstanceMethod(Class cls, SEL origSEL, SEL overrideSEL) {
+//    if (!cls) return;
+//    /* if current class not exist selector, then get super*/
+//    Method originalMethod = class_getInstanceMethod(cls, origSEL);
+//    Method swizzledMethod = class_getInstanceMethod(cls, overrideSEL);
+//
+//    /* add selector if not exist, implement append with method */
+//    if (class_addMethod(cls,
+//                        origSEL,
+//                        method_getImplementation(swizzledMethod),
+//                        method_getTypeEncoding(swizzledMethod)) ) {
+//        /* replace class instance method, added if selector not exist */
+//        /* for class cluster , it always add new selector here */
+//        class_replaceMethod(cls,
+//                            overrideSEL,
+//                            method_getImplementation(originalMethod),
+//                            method_getTypeEncoding(originalMethod));
+//
+//    }else {
+//        /* swizzleMethod maybe belong to super */
+//        class_replaceMethod(cls,
+//                            overrideSEL,
+//                            class_replaceMethod(cls,
+//                                                origSEL,
+//                                                method_getImplementation(swizzledMethod),
+//                                                method_getTypeEncoding(swizzledMethod)),
+//                            method_getTypeEncoding(originalMethod));
+//    }
+//}
+//
+//@implementation NSObject (HIntercept)
+//+ (void)interceptMethodSwizzleWithOrigSEL:(SEL)origSEL overrideSEL:(SEL)overrideSEL {
+//    HInterceptSwizzleInstanceMethod(self.class, origSEL, overrideSEL);
+//}
+//@end
+//
+//
+//@implementation UINavigationController (HIntercept)
+//+ (void)load {
+//    static dispatch_once_t onceToken;
+//    dispatch_once(&onceToken, ^{
+//        [[self class] interceptMethodSwizzleWithOrigSEL:@selector(pushViewController:animated:) overrideSEL:@selector(intercept_pushViewController:animated:)];
+//    });
+//}
+//- (void)intercept_pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
+//    if ([viewController isKindOfClass:HUIViewController.class]) {
+//        if ([viewController isKindOfClass:UIAlertController.class]) return;
+//        SEL selector = NSSelectorFromString(@"viewDidLoad");
+//        if ([viewController respondsToSelector:selector]) {
+//            #pragma clang diagnostic push
+//            #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+//            [viewController performSelector:selector];
+//            #pragma clang diagnostic pop
+//        }
+//    }else {
+//        [self intercept_pushViewController:viewController animated:animated];
+//    }
+//}
+//@end
+//
+//@implementation UIViewController (HIntercept)
+//+ (void)load {
+//    static dispatch_once_t onceToken;
+//    dispatch_once(&onceToken, ^{
+//        [[self class] interceptMethodSwizzleWithOrigSEL:@selector(presentViewController:animated:completion:) overrideSEL:@selector(intercept_presentViewController:animated:completion:)];
+//    });
+//}
+//- (void)intercept_presentViewController:(UIViewController *)viewControllerToPresent animated:(BOOL)flag completion:(void (^ __nullable)(void))completion {
+//    if ([viewControllerToPresent isKindOfClass:HUIViewController.class]) {
+//        if ([viewControllerToPresent isKindOfClass:UIAlertController.class]) return;
+//        SEL selector = NSSelectorFromString(@"viewDidLoad");
+//        if ([viewControllerToPresent respondsToSelector:selector]) {
+//            #pragma clang diagnostic push
+//            #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+//            [viewControllerToPresent performSelector:selector];
+//            #pragma clang diagnostic pop
+//        }
+//    }else {
+//        [self intercept_presentViewController:viewControllerToPresent animated:flag completion:completion];
+//    }
+//}
+//@end
+//
+//@implementation UIAlertView (HIntercept)
+//+ (void)load {
+//    static dispatch_once_t onceToken;
+//    dispatch_once(&onceToken, ^{
+//        [[self class] interceptMethodSwizzleWithOrigSEL:@selector(show) overrideSEL:@selector(intercept_show)];
+//    });
+//}
+//- (void)intercept_show { }
+//@end
+//
+//@implementation HUIViewController (HIntercept)
+//- (UIView *)view {
+//    return nil;
+//}
+//@end
+//
+////该类为要拦截处理的类
+//@implementation HUIViewController
+//
+//@end
