c9a21cd14f22515cfdf198de62c348bb7521dff6
wind <897432098@qq.com>
2020-02-29 16:37:27 +0700

    modify some files

--- QFProj/AppDelegate+RotateService.h
+++ QFProj/AppDelegate+RotateService.h
@@ -8,6 +8,15 @@
 
 #import "AppDelegate.h"
 
+typedef NS_ENUM(NSInteger, UIDeviceOrientationStyle) {
+    UIDeviceOrientationStyleAll, // 横竖屏
+    UIDeviceOrientationStyleVertical, //竖屏
+    UIDeviceOrientationStyleHorizontal, //横屏
+    UIDeviceOrientationStyleHorizontalLeft, //横屏左旋转
+    UIDeviceOrientationStyleHorizontalRight //横屏右旋转
+};
+
 @interface AppDelegate (RotateService)
 @property (nonatomic, class) BOOL shouldAutorotate;
+@property (nonatomic, class) UIDeviceOrientationStyle orientationStyle; //旋转方向
 @end
--- QFProj/AppDelegate+RotateService.m
+++ QFProj/AppDelegate+RotateService.m
@@ -16,11 +16,27 @@
 + (void)setShouldAutorotate:(BOOL)shouldAutorotate {
     objc_setAssociatedObject(self, @selector(shouldAutorotate), @(shouldAutorotate), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
+
++ (UIDeviceOrientationStyle)orientationStyle {
+    return [objc_getAssociatedObject(self, _cmd) integerValue];
+}
++ (void)setOrientationStyle:(UIDeviceOrientationStyle)orientationStyle {
+    objc_setAssociatedObject(self, @selector(orientationStyle), @(orientationStyle), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
 - (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window {
-//    if (AppDelegate.shouldAutorotate) {
-//        return UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskLandscapeLeft | UIInterfaceOrientationMaskLandscapeRight;
-//    }
-//    return UIInterfaceOrientationMaskPortrait;
+    if (AppDelegate.shouldAutorotate) {
+        if (AppDelegate.orientationStyle  == UIDeviceOrientationStyleVertical) {// 竖屏
+            return UIInterfaceOrientationMaskPortrait;
+        }else if (AppDelegate.orientationStyle == UIDeviceOrientationStyleHorizontal) {// 横屏
+            return UIInterfaceOrientationMaskLandscapeLeft | UIInterfaceOrientationMaskLandscapeRight;
+        }else if (AppDelegate.orientationStyle == UIDeviceOrientationStyleHorizontalLeft) {// 横屏左旋转
+            return UIInterfaceOrientationMaskLandscapeLeft;
+        }else if (AppDelegate.orientationStyle == UIDeviceOrientationStyleHorizontalRight) {// 横屏右旋转
+            return UIInterfaceOrientationMaskLandscapeRight;
+        }
+    }
     return UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskLandscapeLeft | UIInterfaceOrientationMaskLandscapeRight;
+    
 }
 @end
