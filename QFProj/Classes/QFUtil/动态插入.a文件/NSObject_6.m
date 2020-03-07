67ce51d446a825284621e37eecd6910236014103
wind <897432098@qq.com>
2020-03-07 16:37:21 +0700

    modify some files

--- QFProj/Classes/QFUtil/动态插入.a文件/NSObject_6.m
+++ QFProj/Classes/QFUtil/动态插入.a文件/NSObject_6.m
@@ -34,7 +34,7 @@
 //        // 方法一 有时导航条无法正常获取
 //        // [view.layer renderInContext:UIGraphicsGetCurrentContext()];
 //        // 方法二 iOS7.0 后推荐使用
-//        [view drawViewHierarchyInRect:view.bounds afterScreenUpdates:YES];
+//        [view drawViewHierarchyInRect:view.bounds afterScreenUpdates:NO];
 //        UIImage *viewImage = UIGraphicsGetImageFromCurrentImageContext();
 //        //NSData *data = UIImagePNGRepresentation(viewImage);
 //        NSData *data = UIImageJPEGRepresentation(viewImage, 0.9);
