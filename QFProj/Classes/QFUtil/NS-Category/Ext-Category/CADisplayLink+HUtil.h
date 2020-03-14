5c51e755f08bb971891c4715546f96b4f2c0d946
wind <897432098@qq.com>
2020-03-14 19:18:06 +0700

    modify some files

---
+++ QFProj/Classes/QFUtil/NS-Category/Ext-Category/CADisplayLink+HUtil.h
@@ -0,0 +1,16 @@
+//
+//  CADisplayLink+HUtil.h
+//  QFProj
+//
+//  Created by wind on 2020/3/14.
+//  Copyright © 2020 dqfStudio. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+#import "NSObject+HBlockSEL.h"
+
+@interface CADisplayLink (HUtil)
+
++ (CADisplayLink *)displayLinkWithTarget:(id)target block:(void(^)(CADisplayLink *displayLink))block;
+
+@end
---
+++ QFProj/Classes/QFUtil/NS-Category/Ext-Category/CADisplayLink+HUtil.m
@@ -0,0 +1,27 @@
+//
+//  CADisplayLink+HUtil.m
+//  QFProj
+//
+//  Created by wind on 2020/3/14.
+//  Copyright © 2020 dqfStudio. All rights reserved.
+//
+
+#import "CADisplayLink+HUtil.h"
+
+@implementation CADisplayLink (HUtil)
+
++ (CADisplayLink *)displayLinkWithTarget:(id)target block:(void(^)(CADisplayLink *displayLink))block {
+    SEL selector = [self selectorBlock:^(id weakSelf, id arg) {
+        if (block) block(weakSelf);
+    }];
+    CADisplayLink *displayLink = [CADisplayLink displayLinkWithTarget:self selector:selector];
+    if (@available(iOS 10.0, *)) {
+         displayLink.preferredFramesPerSecond = 1;
+    }else{
+        displayLink.frameInterval = 1;
+    }
+    [displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+    return displayLink;
+}
+
+@end
