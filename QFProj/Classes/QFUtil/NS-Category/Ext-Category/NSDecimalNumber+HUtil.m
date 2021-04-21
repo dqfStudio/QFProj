ea9372eb8ae560c1af7a1755e8768c89c0b77f0d
Wind <wind@WinddeMacBook-Pro.local>
2021-04-21 18:02:28 +0400

    modify some files

--- QFProj/Classes/QFUtil/NS-Category/Ext-Category/NSDecimalNumber+HUtil.m
+++ QFProj/Classes/QFUtil/NS-Category/Ext-Category/NSDecimalNumber+HUtil.m
@@ -11,7 +11,7 @@
 @implementation NSDecimalNumber (HUtil)
 
 //exclusive默认为YES
-+ (NSString *)notRounding:(float)price afterPoint:(int)position {
++ (NSString *)notRounding:(CGFloat)price afterPoint:(NSInteger)position {
     return [NSDecimalNumber notRounding:price afterPoint:position exclusive:YES];
 }
 
@@ -19,17 +19,33 @@
 //如 price=2.65, position=1，则结果为2.6
 //如 price=3, position=1， exclusive=YES，则结果为3.0
 //如 price=3, position=1， exclusive=NO，则结果为3
-+ (NSString *)notRounding:(float)price afterPoint:(int)position exclusive:(BOOL)exclusive {
++ (NSString *)notRounding:(CGFloat)price afterPoint:(NSInteger)position exclusive:(BOOL)exclusive {
     NSDecimalNumberHandler *roundingBehavior = [NSDecimalNumberHandler decimalNumberHandlerWithRoundingMode:NSRoundDown scale:position raiseOnExactness:NO raiseOnOverflow:NO raiseOnUnderflow:NO raiseOnDivideByZero:NO];
     NSDecimalNumber *ouncesDecimal = [[NSDecimalNumber alloc] initWithFloat:price];
     NSDecimalNumber *roundedOunces = [ouncesDecimal decimalNumberByRoundingAccordingToBehavior:roundingBehavior];
     NSString *priceString = [NSString stringWithFormat:@"%@",roundedOunces];
+    
+    NSRange range = [priceString rangeOfString:@"."];
+    NSInteger length = range.location;
+    if (range.location == NSNotFound ) length = priceString.length;
+    
     if (exclusive) {
         NSMutableString *mutableString = NSMutableString.new;
-        for (int i=0; i<position; i++) {
-            [mutableString appendString:@"0"];
+        if (range.location == NSNotFound) {
+            for (int i=0; i<position; i++) {
+                [mutableString appendString:@"0"];
+            }
+            priceString = [NSString stringWithFormat:@"%@.%@",priceString, mutableString];
+        }else {
+            NSString *subPriceString = [priceString substringFromIndex:range.location+range.length];
+            NSInteger count = position - subPriceString.length;
+            if (subPriceString.length > 0 && count > 0) {
+                for (int i=0; i<count; i++) {
+                    [mutableString appendString:@"0"];
+                }
+                priceString = [NSString stringWithFormat:@"%@%@",priceString, mutableString];
+            }
         }
-        priceString = [NSString stringWithFormat:@"%@.%@",priceString, mutableString];
     }
     return priceString;
 }
