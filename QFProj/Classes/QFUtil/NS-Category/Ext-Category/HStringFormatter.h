1f8170107d4f784815686d95df2da0187858bc79
Wind <wind@WinddeMacBook-Pro.local>
2021-06-01 09:24:07 +0400

    modify some files

--- QFProj/Classes/QFUtil/NS-Category/Ext-Category/HStringFormatter.h
+++ QFProj/Classes/QFUtil/NS-Category/Ext-Category/HStringFormatter.h
@@ -11,16 +11,24 @@
 typedef void(^HTextActionBlock)(NSAttributedString *text, NSRange range);
 
 @interface NSMutableAttributedString (HStringFormatter)
-
+//设置字体大小
 - (void)makeFont:(UIFont *)font range:(NSRange)range;
+//设置字体颜色
 - (void)makeColor:(UIColor *)color range:(NSRange)range;
+//设置字间距
 - (void)makeCharspace:(CGFloat)charspace range:(NSRange)range;
+//设置行间距
 - (void)makeLinespace:(CGFloat)linespace range:(NSRange)range;
+//设置中线
 - (void)makeMiddleline:(NSRange)range;
+//设置下划线
 - (void)makeUnderline:(NSRange)range;
+//设置点击事件
 - (void)makeTapAction:(NSRange)range tapAction:(HTextActionBlock)block;
 
+//追加图片
 - (void)appendImageName:(NSString *)imageName size:(CGSize)size;
+//插入图片
 - (void)insertImageName:(NSString *)imageName size:(CGSize)size atIndex:(NSUInteger)loc;
 
 @end
