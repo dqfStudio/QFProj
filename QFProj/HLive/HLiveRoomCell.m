bcc9bd5242e72f28b3169a3f7191bd28bc704db5
Jovial <Jovial65765@gmail.com>
2021-08-29 13:42:20 +0400

    优化部分代码

--- QFProj/HLive/HLiveRoomCell.m
+++ QFProj/HLive/HLiveRoomCell.m
@@ -68,7 +68,7 @@
     return 1;
 }
 - (NSInteger)numberOfItemsInSection:(NSInteger)section {
-    return 4;
+    return 3;
 }
 - (CGSize)sizeForHeaderInSection:(NSInteger)section {
     return CGSizeMake(self.liveRightView.width, UIScreen.statusBarHeight);
@@ -79,15 +79,14 @@
 - (CGSize)sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
     switch (indexPath.row) {
         case HCell0:
-            return CGSizeMake(self.liveRightView.width, 40);
-            break;
-        case HCell1:
             return CGSizeMake(self.liveRightView.width, 50);
+            break;
+        case HCell1: {
+            NSInteger height = UIScreen.height-50-50-UIScreen.statusBarHeight-UIScreen.bottomBarHeight;
+            return CGSizeMake(self.liveRightView.width, height);
+        }
             break;
         case HCell2:
-            return CGSizeMake(self.liveRightView.width, 50);
-            break;
-        case HCell3:
             return CGSizeMake(self.liveRightView.width, 50);
             break;
             
@@ -112,38 +111,24 @@
         case HCell0: {
             HTupleLabelCell *cell = itemBlock(nil, HTupleLabelCell.class, nil, YES);
             [cell addBottomLineWithSize:1.f color:[UIColor colorWithWhite:0.1 alpha:0.2] paddingLeft:0 paddingRight:0];
-            cell.label.font = [UIFont boldSystemFontOfSize:17.f];
+            cell.label.font = [UIFont boldSystemFontOfSize:14.f];
             cell.label.textAlignment = NSTextAlignmentCenter;
             cell.label.textColor = HColorHex(#0B0A0C);
-            cell.label.text = @"过期提醒";
+            cell.label.text = @"top bar";
         }
             break;
         case HCell1: {
-            HTupleLabelCell *cell = itemBlock(nil, HTupleLabelCell.class, nil, YES);
+            HTupleBlankCell *cell = itemBlock(nil, HTupleBlankCell.class, nil, YES);
             [cell addBottomLineWithSize:1.f color:[UIColor colorWithWhite:0.1 alpha:0.2] paddingLeft:0 paddingRight:0];
-            cell.label.font = [UIFont systemFontOfSize:12.f];
-            cell.label.textAlignment = NSTextAlignmentCenter;
-            cell.label.numberOfLines = 0;
-            cell.label.textColor = HColorHex(#070507);
-            cell.label.text = @"您的会员资格已不足3天，请及时充值!";
         }
             break;
         case HCell2: {
             HTupleLabelCell *cell = itemBlock(nil, HTupleLabelCell.class, nil, YES);
-            [cell addBottomLineWithSize:1.f color:[UIColor colorWithWhite:0.1 alpha:0.2] paddingLeft:0 paddingRight:0];
-            cell.label.font = [UIFont systemFontOfSize:12.f];
+            cell.label.font = [UIFont systemFontOfSize:14.f];
             cell.label.textAlignment = NSTextAlignmentCenter;
             cell.label.numberOfLines = 0;
             cell.label.textColor = HColorHex(#070507);
-            cell.label.text = @"您的会员资格已不足3天，请及时充值!";
-        }
-            break;
-        case HCell3: {
-            HTupleLabelCell *cell = itemBlock(nil, HTupleLabelCell.class, nil, YES);
-            cell.label.font = [UIFont boldSystemFontOfSize:17.f];
-            cell.label.textAlignment = NSTextAlignmentCenter;
-            cell.label.textColor = HColorHex(#3184DD);
-            cell.label.text = @"确定";
+            cell.label.text = @"bottom bar";
         }
             break;
             
