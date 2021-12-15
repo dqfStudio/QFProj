dd6d6d92bb97d6c920e672b3c4c6a3c9ef2b2f3c
Wind <Wind983571@gmail.com>
2021-12-15 16:39:00 +0400

    增加新的功能

---
+++ QFProj/Classes/QFUtil/videoScreenshot/videoScreenshot.sh
@@ -0,0 +1,34 @@
+# !/bin/bash
+
+#for item in `find . -name '*.mp4' -o -name '*.ts' -o -name '*.mkv' -o -name '*.mpg' -o -name '*.mts'`
+#
+#do
+#    ffmpeg -i $item -ss 00:00:05 -vframes 1 -s 140x80 ${item%.*}.jpeg
+#done
+
+
+#1.截取从某一时间开始那一帧缩略图
+#
+#for item in `find . -name '*.mp4' -o -name '*.ts' -o -name '*.mkv' -o -name '*.mpg' -o -name '*.mts'`
+#
+#do
+#    ffmpeg -i $item -ss 00:00:05 -vframes 1 -s 140x80 ${item%.*}.jpeg
+#done
+
+
+
+#2.截取视频所有关键帧缩略图
+
+#for item in `find . -name '*.mp4' -o -name '*.ts' -o -name '*.mkv' -o -name '*.mpg' -o -name '*.mts'`
+#do
+#    ffmpeg -i $item -vf select='eq(pict_type\,I)' -vsync 2 -s 280x160 -f image2 thumbnails-%02d.jpeg
+#
+#done
+
+#3.截取某一关键帧缩略图
+
+for item in `find . -name '*.mp4' -o -name '*.ts' -o -name '*.mkv' -o -name '*.mpg' -o -name '*.mts'`
+do
+    ffmpeg -y -i $item -vf "select=eq(pict_type\\,I)" -vframes 1 -f image2 -s 720x1280 ${item%.*}.jpeg
+#    ffmpeg -y -i $item -ss 115 -t 10 -vf "select=eq(pict_type\\,I)" -vframes 1 -f image2 -s 280x160 ${item%.*}.jpeg
+done
