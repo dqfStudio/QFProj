ea5bd1a2d5b99cc073f456f7f39bfc92bc48ce26
Wind <Wind983571@gmail.com>
2021-12-16 18:40:05 +0400

    增加功能

--- QFProj/Classes/QFUtil/videoScreenshot/videoScreenshot.sh
+++ QFProj/Classes/QFUtil/videoScreenshot/videoScreenshot.sh
@@ -53,3 +53,25 @@ done
 
 #压缩图片
 #ffmpeg -i 屏幕录制.png   -codec libwebp -lossless 0  屏幕录制2.png
+#ffmpeg -i 屏幕录制.png   -codec libwebp -lossless 0 -quality 100  屏幕录制5.png
+
+
+
+
+#-lossless 是设置无损压缩
+#
+#-preset 是几个预设的参数
+#
+#-quality 这个就是主要的参数了，控制压缩率
+#
+#调整图片大小用-vf scale=iw/3:ih/3，可以指定和原来的比例，也可以指定明确的像素值比如1280:720。
+#
+#e.g. 单纯调整图片大小，无损压缩把图片宽高缩小3倍iw/3:ih/3。
+#
+#ffmpeg -i input.png  -vf scale=iw:ih -codec libwebp -lossless 0 -quality 75 out.webp
+#
+#选择无损压缩时，“-lossless -q 100” 是最佳方案,注意:cwebp 仅仅对png格式的图片使用无损压缩时，会有较为高效的压缩率和图片质量
+#选择有损压缩时，“-q 75”是最佳方案（图片质量与体积大小达到均衡）建议其他格式图片使用有损压缩
+#无论何种压缩参数，加上“-m 6”都能使得输出的 WebP 图片进一步减少体积，量级是1%~2%，但是会增加耗时
+#非png格式的图片选择无损压缩时，“-lossless -q 100” 时编码时间长，图片质量反而极高可能变大，编码时间时间很长，cpu使用率飙升跑满，不建议使用
+#建议选择有损压缩时，“-q 75”是最佳方案（图片质量与体积大小达到均衡）建议使用
